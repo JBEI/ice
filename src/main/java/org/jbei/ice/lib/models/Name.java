d38d92b9f5a4c0f407cd2ac63dde4e8443a6a8c0
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2009-12-18 16:16:25 +0000

    convert Name hibernate.xml to persistence annotation
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@61 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/lib/models/Name.java
+++ src/main/java/org/jbei/ice/lib/models/Name.java
@@ -2,11 +2,35 @@ package org.jbei.ice.lib.models;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 import org.jbei.ice.lib.value_objects.INameValueObject;
 
+@Entity
+@Table(name = "names")
+@SequenceGenerator(name = "sequence", sequenceName = "names_id_seq",
+		allocationSize = 1)
 public class Name implements INameValueObject, Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
 	private int id;
+	
+	@Column(name = "name", length = 127, nullable = false)
 	private String name;
+	
+	@ManyToOne
+	@JoinColumn(name = "entries_id", nullable = false, unique=false)
 	private Entry entry;
 
 	public Name() {
