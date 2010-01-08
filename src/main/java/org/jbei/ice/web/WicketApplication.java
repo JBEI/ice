169c373a6bc2765a40160a6ea7a542c3f3ef9df5
zdmytriv <zdmytriv@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-01-08 23:42:10 +0000

    Formatted
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@70 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/web/WicketApplication.java
+++ src/main/java/org/jbei/ice/web/WicketApplication.java
@@ -12,85 +12,80 @@ import org.apache.wicket.request.target.coding.IndexedParamUrlCodingStrategy;
 import org.apache.wicket.settings.ISecuritySettings;
 import org.jbei.ice.lib.authentication.AuthenticationBackend;
 import org.jbei.ice.lib.authentication.LblLdapAuthenticationBackend;
-import org.jbei.ice.lib.authentication.NullAuthenticationBackend;
 import org.jbei.ice.lib.permissions.IceAuthorizationStrategy;
 import org.jbei.ice.lib.utils.JobCue;
 import org.jbei.ice.web.pages.EntryUpdatePage;
 import org.jbei.ice.web.pages.EntryViewPage;
 import org.jbei.ice.web.pages.LogOutPage;
-import org.jbei.ice.web.pages.LoginPage;
 import org.jbei.ice.web.pages.RegisterPage;
 import org.jbei.ice.web.pages.UserEntryPage;
 import org.jbei.ice.web.pages.WelcomePage;
-import org.odlabs.wiquery.core.commons.WiQueryInstantiationListener;
+
+//import org.odlabs.wiquery.core.commons.WiQueryInstantiationListener;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  * 
  * @see org.jbei.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{    
-	
+public class WicketApplication extends WebApplication {
 	private AuthenticationBackend authenticator = null;
-    /**
-     * Constructor
-     */
-	public WicketApplication()
-	{
+
+	/**
+	 * Constructor
+	 */
+	public WicketApplication() {
 	}
-	
+
 	protected void init() {
-		//authenticator = new NullAuthenticator();
+		// authenticator = new NullAuthenticator();
 		authenticator = new LblLdapAuthenticationBackend();
-				
-		mountBookmarkablePage("/login", LoginPage.class);
+
+		mountBookmarkablePage("/login", WelcomePage.class);
 		mountBookmarkablePage("/logout", LogOutPage.class);
 		mountBookmarkablePage("/register", RegisterPage.class);
-		mount(new IndexedParamUrlCodingStrategy("/entry/view", EntryViewPage.class));
-		mount(new IndexedParamUrlCodingStrategy("/entry/update", EntryUpdatePage.class));
+		mount(new IndexedParamUrlCodingStrategy("/entry/view",
+				EntryViewPage.class));
+		mount(new IndexedParamUrlCodingStrategy("/entry/update",
+				EntryUpdatePage.class));
 		mountBookmarkablePage("/user/entries", UserEntryPage.class);
 		mount(new URIRequestTargetUrlCodingStrategy("/static") {
 			@Override
 			public IRequestTarget decode(RequestParameters requestParameters) {
-	                String path = "/static/" + getURI(requestParameters);
-	                return new WebExternalResourceRequestTarget(path);
-	           }
+				String path = "/static/" + getURI(requestParameters);
+				return new WebExternalResourceRequestTarget(path);
+			}
 		});
-		
-		
-		//job cue
+
+		// job cue
 		JobCue jobCue = JobCue.getInstance();
 		Thread jobThread = new Thread(jobCue);
 		jobThread.start();
-		
-		//settings
+
+		// settings
 		ISecuritySettings securitySettings = getSecuritySettings();
 		IceAuthorizationStrategy authorizationStrategy = new IceAuthorizationStrategy();
 		securitySettings.setAuthorizationStrategy(authorizationStrategy);
-		securitySettings.setUnauthorizedComponentInstantiationListener(authorizationStrategy);
-		
-		//wiquery
-		WiQueryInstantiationListener wiQueryInstantiationListener = new WiQueryInstantiationListener();
-		addComponentInstantiationListener(wiQueryInstantiationListener);
+		securitySettings
+				.setUnauthorizedComponentInstantiationListener(authorizationStrategy);
+
+		// wiquery
+		/*
+		 * WiQueryInstantiationListener wiQueryInstantiationListener = new WiQueryInstantiationListener(); addComponentInstantiationListener(wiQueryInstantiationListener);
+		 */
 	}
-	
-	
+
 	@Override
 	public Session newSession(Request request, Response response) {
-		
 		IceSession s = new IceSession(request, response, authenticator);
-		
+
 		return s;
 	}
-	
-		
+
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
-	public Class<WelcomePage> getHomePage()
-	{
+	public Class<WelcomePage> getHomePage() {
 		return WelcomePage.class;
 	}
-
 }
