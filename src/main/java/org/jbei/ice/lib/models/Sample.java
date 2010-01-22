1e8b16831bced3626a5637a2a55a77e1a80d7967
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-01-22 06:59:33 +0000

    Convert Sample to annotations and some fixes
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@111 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/lib/models/Sample.java
+++ src/main/java/org/jbei/ice/lib/models/Sample.java
@@ -2,69 +2,139 @@ package org.jbei.ice.lib.models;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OrderBy;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Cascade;
 import org.jbei.ice.lib.value_objects.ISampleValueObject;
 
+@Entity
+@Table(name = "samples")
+@SequenceGenerator(name = "sequence", sequenceName = "samples_id_seq", allocationSize = 1)
 public class Sample implements ISampleValueObject, Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private int id;
-	private String uuid;
-	private String depositor;
-	private String label;
-	private String notes;
-	private Entry entry;
-	private Date creationTime;
-	private Date modificationTime;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public void setUuid(String uuid) {
-		this.uuid = uuid;
-	}
-	public String getUuid() {
-		return uuid;
-	}
-	public String getDepositor() {
-		return depositor;
-	}
-	public void setDepositor(String depositor) {
-		this.depositor = depositor;
-	}
-	public String getLabel() {
-		return label;
-	}
-	public void setLabel(String label) {
-		this.label = label;
-	}
-	public String getNotes() {
-		return notes;
-	}
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-	public Entry getEntry() {
-		return entry;
-	}
-	public void setEntry(Entry entry) {
-		this.entry = entry;
-	}
-	public Date getCreationTime() {
-		return creationTime;
-	}
-	public void setCreationTime(Date creationTime) {
-		this.creationTime = creationTime;
-	}
-	public Date getModificationTime() {
-		return modificationTime;
-	}
-	public void setModificationTime(Date modificationTime) {
-		this.modificationTime = modificationTime;
-	}
+    private static final long serialVersionUID = 1L;
 
-	
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    private int id;
+
+    @Column(name = "uuid", length = 36)
+    private String uuid;
+
+    @Column(name = "depositor", length = 127)
+    private String depositor;
+
+    @Column(name = "label", length = 127)
+    private String label;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "entries_id", nullable = false, unique = true)
+    private Entry entry;
+
+    @Column(name = "creation_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTime;
+
+    @Column(name = "modification_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modificationTime;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "sample")
+    @Cascade(org.hibernate.annotations.CascadeType.DELETE_ORPHAN)
+    @JoinColumn(name = "samples_id")
+    @OrderBy("id")
+    private Set<Location> locations = new LinkedHashSet<Location>();
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getDepositor() {
+        return depositor;
+    }
+
+    public void setDepositor(String depositor) {
+        this.depositor = depositor;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public Entry getEntry() {
+        return entry;
+    }
+
+    public void setEntry(Entry entry) {
+        this.entry = entry;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    public void setModificationTime(Date modificationTime) {
+        this.modificationTime = modificationTime;
+    }
+
+    public void setLocations(Set<Location> locations) {
+        this.locations = locations;
+    }
+
+    public Set<Location> getLocations() {
+        return locations;
+    }
+
 }
