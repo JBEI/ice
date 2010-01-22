75cca3b4b72664d0c9cff4065f0b901b425bd613
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-01-22 06:59:20 +0000

    Fix locations model  - samples fk was not set to samples_id  - use annotations
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@110 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/lib/models/Location.java
+++ src/main/java/org/jbei/ice/lib/models/Location.java
@@ -3,80 +3,139 @@ package org.jbei.ice.lib.models;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import org.jbei.ice.lib.value_objects.ILocationValueObject;
 
+@Entity
+@Table(name = "locations")
+@SequenceGenerator(name = "sequence", sequenceName = "locations_id_seq", allocationSize = 1)
 public class Location implements ILocationValueObject, Serializable {
-	
-	private int id;
-	private Sample sample;
-	private String location;
-	private String barcode;
-	private String notes;
-	private String wells;
-	private int nColumns;
-	private int nRows;
-	private Date creationTime;
-	private Date modificationTime;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Sample getSample() {
-		return sample;
-	}
-	public void setSample(Sample sample) {
-		this.sample = sample;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	public String getBarcode() {
-		return barcode;
-	}
-	public void setBarcode(String barcode) {
-		this.barcode = barcode;
-	}
-	public String getNotes() {
-		return notes;
-	}
-	public void setNotes(String notes) {
-		this.notes = notes;
-	}
-	public String getWells() {
-		return wells;
-	}
-	public void setWells(String wells) {
-		this.wells = wells;
-	}
-	public int getnColumns() {
-		return nColumns;
-	}
-	public void setnColumns(int nColumns) {
-		this.nColumns = nColumns;
-	}
-	public int getnRows() {
-		return nRows;
-	}
-	public void setnRows(int nRows) {
-		this.nRows = nRows;
-	}
-	public Date getCreationTime() {
-		return creationTime;
-	}
-	public void setCreationTime(Date creationTime) {
-		this.creationTime = creationTime;
-	}
-	public Date getModificationTime() {
-		return modificationTime;
-	}
-	public void setModificationTime(Date modificationTime) {
-		this.modificationTime = modificationTime;
-	}
-	
-	
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequence")
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "samples_id", nullable = false, unique = false)
+    private Sample sample;
+
+    @Column(name = "location", length = 255)
+    private String location;
+
+    @Column(name = "barcode", length = 255)
+    private String barcode;
+
+    @Column(name = "notes")
+    private String notes;
+
+    @Column(name = "wells", length = 255)
+    private String wells;
+
+    @Column(name = "n_columns")
+    private int nColumns;
+
+    @Column(name = "n_rows")
+    private int nRows;
+
+    @Column(name = "creation_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date creationTime;
+
+    @Column(name = "modification_time")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modificationTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Sample getSample() {
+        return sample;
+    }
+
+    public void setSample(Sample sample) {
+        this.sample = sample;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public String getWells() {
+        return wells;
+    }
+
+    public void setWells(String wells) {
+        this.wells = wells;
+    }
+
+    public int getnColumns() {
+        return nColumns;
+    }
+
+    public void setnColumns(int nColumns) {
+        this.nColumns = nColumns;
+    }
+
+    public int getnRows() {
+        return nRows;
+    }
+
+    public void setnRows(int nRows) {
+        this.nRows = nRows;
+    }
+
+    public Date getCreationTime() {
+        return creationTime;
+    }
+
+    public void setCreationTime(Date creationTime) {
+        this.creationTime = creationTime;
+    }
+
+    public Date getModificationTime() {
+        return modificationTime;
+    }
+
+    public void setModificationTime(Date modificationTime) {
+        this.modificationTime = modificationTime;
+    }
+
 }
