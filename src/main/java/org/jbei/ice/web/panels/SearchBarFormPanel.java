0f4e82dcb9f8b430dd8167e3a6aa76ebee7bbf15
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-01-26 09:22:18 +0000

    Fix search bar form regression
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@116 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/web/panels/SearchBarFormPanel.java
+++ src/main/java/org/jbei/ice/web/panels/SearchBarFormPanel.java
@@ -1,70 +1,83 @@
 package org.jbei.ice.web.panels;
 
 import org.apache.wicket.PageParameters;
-import org.apache.wicket.markup.html.form.Button;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.form.AjaxButton;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.StatelessForm;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.Model;
 import org.jbei.ice.web.pages.BlastPage;
 import org.jbei.ice.web.pages.SearchResultPage;
 import org.jbei.ice.web.pages.UserEntryPage;
 
 public class SearchBarFormPanel extends Panel {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public SearchBarFormPanel(String id) {
-		this(id, "");
-	}
-	
-	@SuppressWarnings("unchecked")
-	public SearchBarFormPanel(String id, String queryString) {
-		super(id);
-		
-		class SearchBarForm extends StatelessForm<Object> {
-						
-			private static final long serialVersionUID = 1L;
-			private String searchQuery;
-			
-			public SearchBarForm(String id, String formQueryString) {
-				super(id);
-				setSearchQuery(formQueryString);
-				setModel(new CompoundPropertyModel<Object>(this));
-				add(new TextField<String>("searchQuery"));
-			}
+    public SearchBarFormPanel(String id) {
+        this(id, "");
+    }
 
-			@Override
-			protected void onSubmit() {
-				setRedirect(true);
-				setResponsePage(SearchResultPage.class, new PageParameters(
-						"search=" + getSearchQuery()));
+    @SuppressWarnings("unchecked")
+    public SearchBarFormPanel(String id, String queryString) {
+        super(id);
 
-				System.out.println("submmited: " + getSearchQuery());
-			}
+        class SearchBarForm extends StatelessForm<Object> {
 
-			public void setSearchQuery(String searchQuery) {
-				this.searchQuery = searchQuery;
-			}
+            private static final long serialVersionUID = 1L;
+            private String searchQuery;
 
-			public String getSearchQuery() {
-				return searchQuery;
-			}
-		}
+            public SearchBarForm(String id, String formQueryString) {
+                super(id);
+                setSearchQuery(formQueryString);
+                setModel(new CompoundPropertyModel<Object>(this));
+                add(new TextField<String>("searchQuery"));
+            }
 
-		Form<?> searchBarForm = new SearchBarForm("searchBarForm", queryString);
-		// TODO advanced search
-		searchBarForm.add(new BookmarkablePageLink("advancedSearchLink",
-				UserEntryPage.class));
-		// TODO blast search
-		searchBarForm.add(new BookmarkablePageLink("blastSearchLink",
-				BlastPage.class));
-		searchBarForm.add(new Button("submitButton"));
+            @Override
+            protected void onSubmit() {
 
-		add(searchBarForm);
-	}
+                setRedirect(true);
+                setResponsePage(SearchResultPage.class, new PageParameters("search="
+                        + getSearchQuery()));
 
-	
+                System.out.println("submmited: " + getSearchQuery());
+            }
+
+            public void setSearchQuery(String searchQuery) {
+                this.searchQuery = searchQuery;
+            }
+
+            public String getSearchQuery() {
+                return searchQuery;
+            }
+        }
+
+        Form<?> searchBarForm = new SearchBarForm("searchBarForm", queryString);
+        // TODO advanced search
+        searchBarForm.add(new BookmarkablePageLink("advancedSearchLink", UserEntryPage.class));
+        searchBarForm.add(new BookmarkablePageLink("blastSearchLink", BlastPage.class));
+
+        AjaxButton ajaxButton = new AjaxButton("submitButton", new Model<String>("Search"),
+                searchBarForm) {
+
+            private static final long serialVersionUID = 1L;
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target, Form<?> form) {
+                setRedirect(true);
+                SearchBarForm temp = (SearchBarForm) getParent();
+                String searchQuery = temp.getSearchQuery();
+                setResponsePage(SearchResultPage.class, new PageParameters("search=" + searchQuery));
+            }
+        };
+
+        searchBarForm.add(ajaxButton);
+        add(searchBarForm);
+    }
+
 }
