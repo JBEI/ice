2a18b19138cdc3ae5e1e55c867bb2750b7f27d4e
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-02-01 22:36:36 +0000

    Handle expired sessions gracefully instead of giving the system error page
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@127 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/web/IceRequestCycle.java
+++ src/main/java/org/jbei/ice/web/IceRequestCycle.java
@@ -3,6 +3,7 @@ package org.jbei.ice.web;
 import org.apache.wicket.Page;
 import org.apache.wicket.PageParameters;
 import org.apache.wicket.Response;
+import org.apache.wicket.protocol.http.PageExpiredException;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.protocol.http.WebRequest;
 import org.apache.wicket.protocol.http.WebRequestCycle;
@@ -10,6 +11,7 @@ import org.jbei.ice.lib.logging.Logger;
 import org.jbei.ice.lib.permissions.PermissionException;
 import org.jbei.ice.lib.utils.Utils;
 import org.jbei.ice.web.pages.ErrorPage;
+import org.jbei.ice.web.pages.HomePage;
 import org.jbei.ice.web.pages.PermissionDeniedPage;
 
 public class IceRequestCycle extends WebRequestCycle {
@@ -29,6 +31,10 @@ public class IceRequestCycle extends WebRequestCycle {
             String msg = "Permission violation: " + e.toString();
             Logger.warn(msg);
             result = new PermissionDeniedPage(new PageParameters());
+        } else if (e instanceof PageExpiredException) {
+            // If Page is expired due to forms going stale, etc,
+            // drop the users to their home page
+            result = new HomePage(new PageParameters());
         } else {
             Logger.error(Utils.stackTraceToString(e));
             result = new ErrorPage(e);
