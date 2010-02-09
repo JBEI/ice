02ce3c8885b1b67f838d2f3a28f625a0380900c8
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-02-09 21:08:02 +0000

    Move jbeir settings out to a user editable file
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@178 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/lib/utils/JbeirSettings.java
+++ src/main/java/org/jbei/ice/lib/utils/JbeirSettings.java
@@ -1,50 +1,24 @@
 package org.jbei.ice.lib.utils;
 
-import java.io.File;
-import java.util.HashMap;
+import java.util.ResourceBundle;
+
+import org.jbei.ice.lib.logging.Logger;
 
 public class JbeirSettings {
-    public final static HashMap<String, String> settings = new HashMap<String, String>();
-
-    static {
-        settings.put("DATA_DIRECTORY", "/tmp");
-        settings.put("ATTACHMENTS_DIRECTORY", settings.get("DATA_DIRECTORY") + File.separator
-                + "attachments");
-        settings.put("LOGS_DIRECTORY", settings.get("DATA_DIRECTORY") + File.separator + "logs");
-        settings.put("LUCENE_DIRECTORY", settings.get("DATA_DIRECTORY"));
-        settings.put("BLAST_DIRECTORY", settings.get("DATA_DIRECTORY") + File.separator + "blast");
-        settings.put("SITE_SECRET", "Secret Sauce");
-        settings.put("COOKIE_NAME", "gd-ice");
-        settings.put("SEARCH_INDEX_FILE", settings.get("DATA_DIRECTORY") + File.separator
-                + "ice_search_index");
-        settings.put("ADMIN_EMAIL", "tsham@lbl.gov");
-        settings.put("MODERATOR_EMAIL", "tsham@lbl.gov");
-        settings.put("SMTP_HOST", "smtp.lbl.gov");
-        settings.put("ERROR_EMAIL_EXCEPTION_PREFIX", "[ERROR] ");
-        settings.put("PROJECT_NAME", "JBEI Registry");
-        settings.put("SECRET_KEY", "o6-v(yay5w@0!64e6-+ylbhcd9g03rv#@ezqh7axchds=q=$n+");
-        settings.put("PART_NUMBER_PREFIX", "TEST");
-        settings.put("PART_NUMBER_DIGITAL_SUFFIX", "000001");
-        settings.put("PART_NUMBER_DELIMITER", "_");
-        settings.put("JOB_CUE_DELAY", "300000"); // 300000ms = 5 minutes"
-        settings.put("BLAST_BLASTALL", "/usr/bin/blastall");
-        settings.put("BLAST_FORMATDB", "/usr/bin/formatdb");
-        settings.put("BLAST_DATABASE_NAME", "jbeiblast");
-        // LocalBackend, NullAuthenticationBackend, LblLdapAuthenticationBackend are built-in
-        settings.put("AUTHENTICATION_BACKEND",
-                "org.jbei.ice.lib.authentication.LblLdapAuthenticationBackend");
-        settings.put("NEW_REGISTRATION_ALLOWED", "no");
-    }
 
     public static String getSetting(String key) {
-        String result = null;
-
-        if (settings.containsKey(key)) {
-            result = settings.get(key);
-        } else {
-            result = "";
+        String result = "";
+        try {
+            ResourceBundle resource = ResourceBundle.getBundle("jbeir");
+            String tmp = resource.getString(key).trim();
+            if (tmp != null) {
+                result = tmp;
+            }
+        } catch (Exception e) {
+            String msg = "Could not read properties file: " + e.toString();
+            Logger.error(msg);
         }
-
         return result;
     }
+
 }
