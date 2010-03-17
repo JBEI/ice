c776288366545f8e5f1413c273c55fde48fd9791
zdmytriv <zdmytriv@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-03-17 19:34:40 +0000

    refactored: BaseService for blazeds
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@335 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/services/blazeds/common/BaseService.java
+++ src/main/java/org/jbei/ice/services/blazeds/common/BaseService.java
@@ -1,14 +1,21 @@
 package org.jbei.ice.services.blazeds.common;
 
+import org.jbei.ice.controllers.AccountController;
+import org.jbei.ice.controllers.common.ControllerException;
 import org.jbei.ice.lib.logging.Logger;
-import org.jbei.ice.lib.managers.AccountManager;
 import org.jbei.ice.lib.models.Account;
 
 public class BaseService {
     public static final String BASE_SERVICE_NAME = "BlazeDS";
 
     protected Account getAccountByToken(String authToken) {
-        Account account = AccountManager.getAccountByAuthToken(authToken);
+        Account account = null;
+
+        try {
+            account = AccountController.getAccountByAuthToken(authToken);
+        } catch (ControllerException e) {
+            Logger.error("Failed to get account by token: " + authToken, e);
+        }
 
         if (account == null) {
             Logger.warn(getServiceName() + "User by token doesn't exist: " + authToken);
