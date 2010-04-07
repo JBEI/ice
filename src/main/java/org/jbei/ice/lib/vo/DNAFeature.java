9d48ee583d820d909af684a9f24bb57892f28c65
zdmytriv <zdmytriv@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-04-07 03:12:22 +0000

    created DNAFeature and FeatureDNASequence classes for easier sequence manipulation
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@386 d7922f76-c635-11de-8f3f-2f730f80caef

---
+++ src/main/java/org/jbei/ice/lib/vo/DNAFeature.java
@@ -0,0 +1,80 @@
+package org.jbei.ice.lib.vo;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class DNAFeature implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private int start = 0;
+    private int end = 0;
+    private String type = "";
+    private String name = "";
+    private int strand = 1;
+    private Map<String, String> notes = new LinkedHashMap<String, String>();
+
+    public DNAFeature() {
+        super();
+    }
+
+    public DNAFeature(int start, int end, String type, String name, int strand,
+            Map<String, String> notes) {
+        super();
+
+        this.start = start;
+        this.end = end;
+        this.type = type;
+        this.name = name;
+        this.strand = strand;
+        this.notes = notes;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Map<String, String> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(Map<String, String> notes) {
+        this.notes = notes;
+    }
+
+    public int getStrand() {
+        return strand;
+    }
+
+    public void setStrand(int strand) {
+        this.strand = strand;
+    }
+}
---
+++ src/main/java/org/jbei/ice/lib/vo/FeaturedDNASequence.java
@@ -0,0 +1,70 @@
+package org.jbei.ice.lib.vo;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class FeaturedDNASequence implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private List<DNAFeature> features = new LinkedList<DNAFeature>();
+    private String sequence = "";
+    private String accessionNumber = "";
+    private String identifier = "";
+
+    public FeaturedDNASequence() {
+        super();
+    }
+
+    public FeaturedDNASequence(String sequence, List<DNAFeature> features) {
+        super();
+
+        this.features = features;
+        this.sequence = sequence;
+    }
+
+    public FeaturedDNASequence(String sequence, List<DNAFeature> features, String accessionNumber,
+            String identifier) {
+        super();
+
+        this.features = features;
+        this.sequence = sequence;
+        this.accessionNumber = accessionNumber;
+        this.identifier = identifier;
+    }
+
+    public List<DNAFeature> getFeatures() {
+        return features;
+    }
+
+    public void setFeatures(List<DNAFeature> features) {
+        this.features = features;
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getAccessionNumber() {
+        return accessionNumber;
+    }
+
+    public void setAccessionNumber(String accessionNumber) {
+        this.accessionNumber = accessionNumber;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+}
