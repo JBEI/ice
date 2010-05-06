ae743a2658fc6e1f674f859185ac35fe1da92c63
zdmytriv <zdmytriv@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-05-06 16:42:39 +0000

    new restriction enzymes REBASE parsing bio library
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@454 d7922f76-c635-11de-8f3f-2f730f80caef

---
+++ src/main/java/org/jbei/ice/bio/enzymes/RestrictionEnzymesManager.java
@@ -0,0 +1,80 @@
+package org.jbei.ice.bio.enzymes;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Set;
+
+import org.biojava.bio.BioException;
+import org.biojava.bio.molbio.RestrictionEnzymeManager;
+
+public class RestrictionEnzymesManager {
+    private static final String REBASE_DATABASE_RESOURCE_FILE = "/org/jbei/ice/bio/enzymes/link_withrefm";
+
+    public static RestrictionEnzymesManager instance = null;
+
+    private HashMap<String, RestrictionEnzyme> enzymesMap = new HashMap<String, RestrictionEnzyme>();
+
+    private RestrictionEnzymesManager() throws RestrictionEnzymesManagerException {
+        loadEnzymes();
+    }
+
+    public static RestrictionEnzymesManager getInstance() throws RestrictionEnzymesManagerException {
+        if (instance == null) {
+            instance = new RestrictionEnzymesManager();
+        }
+
+        return instance;
+    }
+
+    public Collection<RestrictionEnzyme> getEnzymes() {
+        return enzymesMap.values();
+    }
+
+    public RestrictionEnzyme getEnzymeByName(String enzymeName) {
+        if (enzymeName == null || enzymeName.isEmpty()) {
+            return null;
+        }
+
+        RestrictionEnzyme lookupEnzyme = null;
+
+        if (enzymesMap.containsKey(enzymeName)) {
+            lookupEnzyme = enzymesMap.get(enzymeName);
+        }
+
+        return lookupEnzyme;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void loadEnzymes() throws RestrictionEnzymesManagerException {
+        InputStream is = RestrictionEnzymesManager.class
+                .getResourceAsStream(REBASE_DATABASE_RESOURCE_FILE);
+
+        try {
+            RestrictionEnzymeManager.loadEnzymeFile(is, false);
+
+            Set<org.biojava.bio.molbio.RestrictionEnzyme> reSet = (Set<org.biojava.bio.molbio.RestrictionEnzyme>) RestrictionEnzymeManager
+                    .getAllEnzymes();
+
+            for (org.biojava.bio.molbio.RestrictionEnzyme re : reSet) {
+                RestrictionEnzyme restrictionEnzyme = null;
+
+                if (re.getCutType() == org.biojava.bio.molbio.RestrictionEnzyme.CUT_COMPOUND) {
+                    restrictionEnzyme = new RestrictionEnzyme(re.getName(), re.getRecognitionSite()
+                            .seqString(), re.getCutType(), re.getForwardRegex(), re
+                            .getReverseRegex(), re.getDownstreamCut()[0], re.getDownstreamCut()[1],
+                            re.getUpstreamCut()[0], re.getUpstreamCut()[1]);
+                } else {
+                    restrictionEnzyme = new RestrictionEnzyme(re.getName(), re.getRecognitionSite()
+                            .seqString(), re.getCutType(), re.getForwardRegex(), re
+                            .getReverseRegex(), re.getDownstreamCut()[0], re.getDownstreamCut()[1],
+                            -1, -1);
+                }
+
+                enzymesMap.put(restrictionEnzyme.getName(), restrictionEnzyme);
+            }
+        } catch (BioException e) {
+            throw new RestrictionEnzymesManagerException("Failed to cache restriction enzymes!", e);
+        }
+    }
+}
---
+++ src/main/java/org/jbei/ice/bio/enzymes/RestrictionEnzymesManagerException.java
@@ -0,0 +1,20 @@
+package org.jbei.ice.bio.enzymes;
+
+public class RestrictionEnzymesManagerException extends Exception {
+    private static final long serialVersionUID = -7985110598540062855L;
+
+    public RestrictionEnzymesManagerException() {
+    }
+
+    public RestrictionEnzymesManagerException(String arg0) {
+        super(arg0);
+    }
+
+    public RestrictionEnzymesManagerException(Throwable arg0) {
+        super(arg0);
+    }
+
+    public RestrictionEnzymesManagerException(String arg0, Throwable arg1) {
+        super(arg0, arg1);
+    }
+}
