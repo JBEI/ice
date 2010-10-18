ae4039f51feaf7ef3c45b01793cde2e573f6affe
timothyham <timothyham@d7922f76-c635-11de-8f3f-2f730f80caef>
2010-10-18 22:21:22 +0000

    New VE that sends context info on copy
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@599 d7922f76-c635-11de-8f3f-2f730f80caef

--- src/main/java/org/jbei/ice/web/pages/static/ve/history/history.js
+++ src/main/java/org/jbei/ice/web/pages/static/ve/history/history.js
@@ -19,6 +19,7 @@ BrowserHistory = (function() {
     // type of browser
     var browser = {
         ie: false, 
+        ie8: false, 
         firefox: false, 
         safari: false, 
         opera: false, 
@@ -67,6 +68,11 @@ BrowserHistory = (function() {
     } else if (useragent.indexOf("msie") != -1) {
         browser.ie = true;
         browser.version = parseFloat(useragent.substring(useragent.indexOf('msie') + 4));
+        if (browser.version == 8)
+        {
+            browser.ie = false;
+            browser.ie8 = true;
+        }
     } else if (useragent.indexOf("safari") != -1) {
         browser.safari = true;
         browser.version = parseFloat(useragent.substring(useragent.indexOf('safari') + 7));
@@ -76,6 +82,21 @@ BrowserHistory = (function() {
 
     if (browser.ie == true && browser.version == 7) {
         window["_ie_firstload"] = false;
+    }
+
+    function hashChangeHandler()
+    {
+        currentHref = document.location.href;
+        var flexAppUrl = getHash();
+        //ADR: to fix multiple
+        if (typeof BrowserHistory_multiple != "undefined" && BrowserHistory_multiple == true) {
+            var pl = getPlayers();
+            for (var i = 0; i < pl.length; i++) {
+                pl[i].browserURLChange(flexAppUrl);
+            }
+        } else {
+            getPlayer().browserURLChange(flexAppUrl);
+        }
     }
 
     // Accessor functions for obtaining specific elements of the page.
@@ -102,6 +123,8 @@ BrowserHistory = (function() {
     // Get the Flash player object for performing ExternalInterface callbacks.
     // Updated for changes to SWFObject2.
     function getPlayer(id) {
+        var i;
+
 		if (id && document.getElementById(id)) {
 			var r = document.getElementById(id);
 			if (typeof r.SetVariable != "undefined") {
@@ -110,40 +133,53 @@ BrowserHistory = (function() {
 			else {
 				var o = r.getElementsByTagName("object");
 				var e = r.getElementsByTagName("embed");
-				if (o.length > 0 && typeof o[0].SetVariable != "undefined") {
-					return o[0];
-				}
-				else if (e.length > 0 && typeof e[0].SetVariable != "undefined") {
-					return e[0];
-				}
+                for (i = 0; i < o.length; i++) {
+                    if (typeof o[i].browserURLChange != "undefined")
+                        return o[i];
+                }
+                for (i = 0; i < e.length; i++) {
+                    if (typeof e[i].browserURLChange != "undefined")
+                        return e[i];
+                }
 			}
 		}
 		else {
 			var o = document.getElementsByTagName("object");
 			var e = document.getElementsByTagName("embed");
-			if (e.length > 0 && typeof e[0].SetVariable != "undefined") {
-				return e[0];
-			}
-			else if (o.length > 0 && typeof o[0].SetVariable != "undefined") {
-				return o[0];
-			}
-			else if (o.length > 1 && typeof o[1].SetVariable != "undefined") {
-				return o[1];
-			}
+            for (i = 0; i < e.length; i++) {
+                if (typeof e[i].browserURLChange != "undefined")
+                {
+                    return e[i];
+                }
+            }
+            for (i = 0; i < o.length; i++) {
+                if (typeof o[i].browserURLChange != "undefined")
+                {
+                    return o[i];
+                }
+            }
 		}
 		return undefined;
 	}
     
     function getPlayers() {
+        var i;
         var players = [];
         if (players.length == 0) {
             var tmp = document.getElementsByTagName('object');
-            players = tmp;
+            for (i = 0; i < tmp.length; i++)
+            {
+                if (typeof tmp[i].browserURLChange != "undefined")
+                    players.push(tmp[i]);
+            }
         }
-        
         if (players.length == 0 || players[0].object == null) {
             var tmp = document.getElementsByTagName('embed');
-            players = tmp;
+            for (i = 0; i < tmp.length; i++)
+            {
+                if (typeof tmp[i].browserURLChange != "undefined")
+                    players.push(tmp[i]);
+            }
         }
         return players;
     }
@@ -241,8 +277,7 @@ BrowserHistory = (function() {
                 historyHash[history.length] = flexAppUrl;
                 _storeStates();
             } else {
-                // Otherwise, write an anchor into the page and tell the browser to go there
-                addAnchor(flexAppUrl);
+                // Otherwise, just tell the browser to go there
                 setHash(flexAppUrl);
             }
         }
@@ -298,6 +333,7 @@ BrowserHistory = (function() {
 						// this.iframe.src = this.blankURL + hash;
 						var sourceToSet = historyFrameSourcePrefix + getHash();
 						getHistoryFrame().src = sourceToSet;
+                        currentHref = document.location.href;
 					}
                 }
             }
@@ -307,14 +343,17 @@ BrowserHistory = (function() {
             // For Safari, we have to check to see if history.length changed.
             if (currentHistoryLength >= 0 && history.length != currentHistoryLength) {
                 //alert("did change: " + history.length + ", " + historyHash.length + "|" + historyHash[history.length] + "|>" + historyHash.join("|"));
-                // If it did change, then we have to look the old state up
-                // in our hand-maintained array since document.location.hash
-                // won't have changed, then call back into BrowserManager.
+                var flexAppUrl = getHash();
+                if (browser.version < 528.16 /* Anything earlier than Safari 4.0 */)
+                {    
+                    // If it did change and we're running Safari 3.x or earlier, 
+                    // then we have to look the old state up in our hand-maintained 
+                    // array since document.location.hash won't have changed, 
+                    // then call back into BrowserManager.
                 currentHistoryLength = history.length;
-                var flexAppUrl = historyHash[currentHistoryLength];
-                if (flexAppUrl == '') {
-                    //flexAppUrl = defaultHash;
+                    flexAppUrl = historyHash[currentHistoryLength];
                 }
+
                 //ADR: to fix multiple
                 if (typeof BrowserHistory_multiple != "undefined" && BrowserHistory_multiple == true) {
                     var pl = getPlayers();
@@ -386,9 +425,6 @@ BrowserHistory = (function() {
                 // Firefox changed; do a callback into BrowserManager to tell it.
                 currentHref = document.location.href;
                 var flexAppUrl = getHash();
-                if (flexAppUrl == '') {
-                    //flexAppUrl = defaultHash;
-                }
                 //ADR: to fix multiple
                 if (typeof BrowserHistory_multiple != "undefined" && BrowserHistory_multiple == true) {
                     var pl = getPlayers();
@@ -401,14 +437,6 @@ BrowserHistory = (function() {
             }
         }
         //setTimeout(checkForUrlChange, 50);
-    }
-
-    /* Write an anchor into the page to legitimize it as a URL for Firefox et al. */
-    function addAnchor(flexAppUrl)
-    {
-       if (document.getElementsByName(flexAppUrl).length == 0) {
-           getAnchorElement().innerHTML += "<a name='" + flexAppUrl + "'>" + flexAppUrl + "</a>";
-       }
     }
 
     var _initialize = function () {
@@ -426,7 +454,8 @@ BrowserHistory = (function() {
             var iframe = document.createElement("iframe");
             iframe.id = 'ie_historyFrame';
             iframe.name = 'ie_historyFrame';
-            //iframe.src = historyFrameSourcePrefix;
+            iframe.src = 'javascript:false;'; 
+
             try {
                 document.body.appendChild(iframe);
             } catch(e) {
@@ -467,13 +496,15 @@ BrowserHistory = (function() {
 
         }
 
-        if (browser.firefox)
+        if (browser.firefox || browser.ie8)
         {
             var anchorDiv = document.createElement("div");
             anchorDiv.id = 'firefox_anchorDiv';
             document.body.appendChild(anchorDiv);
         }
-        
+
+        if (browser.ie8)        
+            document.body.onhashchange = hashChangeHandler;
         //setTimeout(checkForUrlChange, 50);
     }
 
