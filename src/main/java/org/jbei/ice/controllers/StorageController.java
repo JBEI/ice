13c751faa58f3cc95f191c01b28171e47e6128be
hplahar.jbei@gmail.com <hplahar.jbei@gmail.com@d7922f76-c635-11de-8f3f-2f730f80caef>
2011-04-04 21:08:04 +0000

    facilities for importing keio collection from csv, adding controller for storage
    git-svn-id: https://gd-ice.googlecode.com/svn/trunk/ice@703 d7922f76-c635-11de-8f3f-2f730f80caef

---
+++ src/main/java/org/jbei/ice/controllers/StorageController.java
@@ -0,0 +1,50 @@
+package org.jbei.ice.controllers;
+
+import org.jbei.ice.controllers.common.Controller;
+import org.jbei.ice.controllers.common.ControllerException;
+import org.jbei.ice.controllers.permissionVerifiers.SamplePermissionVerifier;
+import org.jbei.ice.lib.managers.ManagerException;
+import org.jbei.ice.lib.managers.StorageManager;
+import org.jbei.ice.lib.models.Account;
+import org.jbei.ice.lib.models.Storage;
+import org.jbei.ice.lib.models.Storage.StorageType;
+
+public class StorageController extends Controller {
+
+    public StorageController(Account account) {
+        super(account, new SamplePermissionVerifier());
+    }
+
+    public Storage retrieveStorageBy(String name, String index, StorageType type, long parentId)
+            throws ControllerException {
+        try {
+            return StorageManager.retrieveStorageBy(name, index, type, parentId);
+        } catch (ManagerException e) {
+            throw new ControllerException(e);
+        }
+    }
+
+    public Storage retrieveStorageTube(String barcode) throws ControllerException {
+        try {
+            return StorageManager.retrieveStorageTube(barcode);
+        } catch (ManagerException me) {
+            throw new ControllerException(me);
+        }
+    }
+
+    public Storage update(Storage storage) throws ControllerException {
+        try {
+            return StorageManager.update(storage);
+        } catch (ManagerException e) {
+            throw new ControllerException(e);
+        }
+    }
+
+    public Storage save(Storage storage) throws ControllerException {
+        try {
+            return StorageManager.save(storage);
+        } catch (ManagerException e) {
+            throw new ControllerException(e);
+        }
+    }
+}
