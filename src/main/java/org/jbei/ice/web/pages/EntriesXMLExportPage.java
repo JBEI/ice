5b1114c5a30e96d63c6240bfc69b9aec81b9addb
Timothy Ham <tsham@lbl.gov>
2011-08-09 15:58:40 -0700

    Fix bug in xml exl export page

--- src/main/java/org/jbei/ice/web/pages/EntriesXMLExportPage.java
+++ src/main/java/org/jbei/ice/web/pages/EntriesXMLExportPage.java
@@ -1,11 +1,7 @@
 package org.jbei.ice.web.pages;
 
-import java.io.IOException;
-import java.io.StringWriter;
 import java.util.ArrayList;
 
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
 import org.jbei.ice.lib.models.Entry;
 import org.jbei.ice.lib.utils.IceXmlSerializer;
 import org.jbei.ice.lib.utils.UtilityException;
@@ -24,19 +20,9 @@ public class EntriesXMLExportPage extends XMLExportPage {
     public String getContent() {
 
         try {
-            //return IceXmlSerializer.serializeToJbeiXml(entries).asXML();
-            OutputFormat format = OutputFormat.createPrettyPrint();
-            XMLWriter xmlWriter;
-            StringWriter stringWriter = new StringWriter();
-
-            xmlWriter = new XMLWriter(stringWriter, format);
-            xmlWriter.write(IceXmlSerializer.serializeToJbeiXml(entries));
-
-            return stringWriter.toString();
+            return IceXmlSerializer.serializeToJbeiXml(entries);
 
         } catch (UtilityException e) {
-            throw new ViewException(e);
-        } catch (IOException e) {
             throw new ViewException(e);
         }
     }
