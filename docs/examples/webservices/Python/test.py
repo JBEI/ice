a2a1d02bbcdd8e4ebe609095484e52a43892cccc
Timothy Ham <tsham@lbl.gov>
2011-11-03 13:19:21 -0700

    update example python soap client

--- docs/examples/webservices/Python/test.py
+++ docs/examples/webservices/Python/test.py
@@ -3,7 +3,48 @@ from RegistryAPIService_types import ns0
 
 import sys
 
-def testTraceSequenceUpload(sessionId):
+def testGetEntry(sessionId):
+    request = getByPartNumber()
+    request._sessionId = sessionId
+    request._partNumber = "JBx_000902"
+
+    response = portType.getByPartNumber(request)
+
+    #recordId = dir(response.Return)
+    #print dir(response.Return)
+    print response.Return.PartNumbers[0].PartNumber
+    return response.Return.RecordId
+
+def testlistTraceFiles(sessionId, recordId):
+    list_request = listTraceSequenceFiles()
+    list_request._sessionId = sessionId
+    list_request._recordId = recordId
+
+    response = portType.listTraceSequenceFiles(list_request)
+    print dir(response.Return[0])
+    print response.Return[0].Filename
+
+def testGetTraceFile(sessionId):
+    recordId = testGetEntry(sessionId)
+
+    list_request = listTraceSequenceFiles()
+    list_request._sessionId = sessionId
+    list_request._recordId = recordId
+
+    response = portType.listTraceSequenceFiles(list_request)
+
+    fileId = response.Return[-1]
+    
+    print "fileId " + fileId
+
+    request = getTraceSequenceFile()
+    request._sessionId = sessionId
+    request._fileId = fileId
+
+    response = portType.getTraceSequenceFile(request)
+    print dir(response.Return)
+    
+def testSequenceTraceUpload(sessionId):
     request = getByPartNumber()
     request._sessionId = sessionId
     request._partNumber = "JBx_000902"
@@ -19,6 +60,17 @@ def testTraceSequenceUpload(sessionId):
     response = portType.listTraceSequenceFiles(list_request)
 
     print response.Return
+
+    ###
+    #not_my_file = response.Return[0]
+    #
+    #request = deleteTraceSequenceFile()
+    #request._sessionId = sessionId
+    #request._fileId = not_my_file
+    #
+    #response = portType.deleteTraceSequenceFile(request)
+
+    #print response.Return
     
     # new file
     fileName = "RDR008_RR01_2010-07-24_G10.ab1"
@@ -37,19 +89,18 @@ def testTraceSequenceUpload(sessionId):
 
     print newFileId
 
-    response = portType.listTraceSequenceFiles(list_request)
-    print response.Return
+#    response = portType.listTraceSequenceFiles(list_request)
+#    print response.Return
     
     
     delete_request = deleteTraceSequenceFile()
     delete_request._sessionId = sessionId
     delete_request._fileId = newFileId
 
-    response = portType.deleteTraceSequenceFile(delete_request)
-    print portType.listTraceSequenceFiles(list_request).Return
+#    response = portType.deleteTraceSequenceFile(delete_request)
+#    print portType.listTraceSequenceFiles(list_request).Return
     
-    response = portType.listTraceSequenceFiles(list_request)
-    print response.Return
+
 
 if __name__ == "__main__":
     registryAPIServiceLocator = RegistryAPIServiceLocator()
@@ -58,9 +109,16 @@ if __name__ == "__main__":
     portType = registryAPIServiceLocator.getRegistryAPIPort(**kw)
     
     request = login()
-    request._login = 'YOUR_USER_NAME_GOES_HERE'
+    request._login = 'tsham@lbl.gov'
     request._password = 'YOUR_PASSWORD_GOES_HERE'
     
     response = portType.login(request)
-    print response.Return
+    sessionId = response.Return
+
+    print "sessionID: " + sessionId
+
+    #recordId = testGetEntry(sessionId)
+    #testlistTraceFiles(sessionId, recordId)
+    #testSequenceTraceUpload(sessionId)
+    #testGetTraceFile(sessionId)
     
