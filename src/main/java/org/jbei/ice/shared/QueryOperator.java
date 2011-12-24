b0fcbae5f6c5b7182916dbb3f11b06b68c82823a
Hector Plahar <hplahar.jbei@gmail.com>
2011-12-24 10:06:53 -0800

    java doc and overriding toString()

--- src/main/java/org/jbei/ice/shared/QueryOperator.java
+++ src/main/java/org/jbei/ice/shared/QueryOperator.java
@@ -1,16 +1,27 @@
 package org.jbei.ice.shared;
 
+/**
+ * Operators for advanced search.
+ * 
+ * @author Hector Plahar
+ */
 public enum QueryOperator {
 
     IS("is", "="), CONTAINS("contains", "~"), DOES_NOT_CONTAIN("doesn't contain", "!~"), BEGINS_WITH(
             "begins with", "^"), ENDS_WITH("ends with", "$"), IS_NOT("isn't", "!"), BOOLEAN(
-            "yes_no", null);
+            "yes_no", ":");
 
     // Not properly represented here is "yes" "no"
 
     private String operator;
     private String value;
 
+    /**
+     * @param operator
+     *            String value of operator
+     * @param value
+     *            operator char symbol
+     */
     QueryOperator(String operator, String value) {
         this.operator = operator;
         this.value = value;
@@ -18,6 +29,11 @@ public enum QueryOperator {
 
     public String operator() {
         return operator;
+    }
+
+    @Override
+    public String toString() {
+        return this.operator;
     }
 
     public String value() {
