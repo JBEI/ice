67e684f21bf3f1df9aeb8c2ce48dd00f64162851
Timothy Ham <tsham@lbl.gov>
2012-02-17 16:01:20 -0800

    Prevent crash on admin edit parts page

--- src/main/java/org/jbei/ice/web/panels/adminpage/AdminPartsDataViewPanel.java
+++ src/main/java/org/jbei/ice/web/panels/adminpage/AdminPartsDataViewPanel.java
@@ -30,7 +30,7 @@ public class AdminPartsDataViewPanel extends Panel {
 
 	private static final long serialVersionUID = 1L;
 	private UserEntriesDataProvider sortableDataProvider;
-	
+
 	ResourceReference blankImage;
     ResourceReference hasAttachmentImage;
     ResourceReference hasSequenceImage;
@@ -38,7 +38,7 @@ public class AdminPartsDataViewPanel extends Panel {
 
 	public AdminPartsDataViewPanel(String id) {
 		super(id);
-		
+
 		Account systemAccount;
 		try {
 			systemAccount = AccountController.getSystemAccount();
@@ -46,7 +46,7 @@ public class AdminPartsDataViewPanel extends Panel {
 			throw new ViewException(e);
 		}
 		sortableDataProvider = new UserEntriesDataProvider(systemAccount);
-		
+
 		blankImage = new ResourceReference(UnprotectedPage.class,
                 UnprotectedPage.IMAGES_RESOURCE_LOCATION + "blank.png");
         hasAttachmentImage = new ResourceReference(UnprotectedPage.class,
@@ -64,12 +64,12 @@ public class AdminPartsDataViewPanel extends Panel {
         add(new Image("attachmentHeaderImage", hasAttachmentImage));
         add(new Image("sequenceHeaderImage", hasSequenceImage));
         add(new Image("sampleHeaderImage", hasSampleImage));
-        
+
         SystemAssignedEntriesDataView dataView = new SystemAssignedEntriesDataView("entriesDataView",sortableDataProvider, 50);
 		add(dataView);
 		add(new JbeiPagingNavigator("navigator", dataView));
 	}
-	
+
 	private class SystemAssignedEntriesDataView extends AbstractEntriesDataView<Entry> {
 
 		private static final long serialVersionUID = 1L;
@@ -78,31 +78,32 @@ public class AdminPartsDataViewPanel extends Panel {
 				UserEntriesDataProvider dataProvider, int perPage) {
 			super(id, dataProvider, perPage);
 		}
-		
+
 		@Override
 		protected void populateItem(Item<Entry> item) {
 			super.populateItem(item);
-			
+
 			// add additional columns
 			renderUndelete(item);
 		}
-		
+
 		private void renderUndelete(final Item<Entry> item) {
 			final Entry entry = item.getModelObject();
 			final String prevOwner = parseOutPreviousOwner(entry).trim();
-			
+
 			AjaxLink<Object> deleteLink = new AjaxLink<Object>("undelete_link") {
-				
+
                 private static final long serialVersionUID = 1L;
 
                 @Override
 				public void onClick(AjaxRequestTarget target) {
-					
-					if (prevOwner == null)
-						throw new ViewException("Could not retrieve previous owner!");
-					
+
+					if (prevOwner == null) {
+                        throw new ViewException("Could not retrieve previous owner!");
+                    }
+
 					entry.setOwnerEmail(prevOwner);
-					
+
 					try {
 						EntryManager.save(entry);
 						JobCue.getInstance().addJob(Job.REBUILD_BLAST_INDEX);
@@ -110,23 +111,29 @@ public class AdminPartsDataViewPanel extends Panel {
 					} catch (ManagerException e) {
 						throw new ViewException(e);
 					}
-					
+
 					setResponsePage(AdminPage.class, new PageParameters("0=parts"));
 				}
 	        };
 
 	        deleteLink.add(new JavascriptEventConfirmation("onclick", "Restore entry \\'" + entry.getPartNumbersAsString() + "\\' to previous owner \\'" + prevOwner + "\\'?"));
 	        deleteLink.add(new Label("undelete_link_label","undelete"));
-	        item.add(deleteLink);   
+	        item.add(deleteLink);
 		}
-		
+
 		private String parseOutPreviousOwner(Entry entry) {
 			String description = entry.getLongDescription();
 			String[] split = description.split("\n");
-			if (split.length < 1)
-				return null;
-			
-			String prevOwner = split[0].substring(split[0].lastIndexOf(' '));
+            String prevOwner = "unknown";
+			if (split.length < 1) {
+                return prevOwner;
+            }
+
+            if (split[0].lastIndexOf(' ') != -1) {
+                prevOwner = split[0].substring(split[0].lastIndexOf(' '));
+            } else {
+                return prevOwner;
+            }
 			return prevOwner;
 		}
 
