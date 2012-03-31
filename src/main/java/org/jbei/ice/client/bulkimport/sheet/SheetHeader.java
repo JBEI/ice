631d875326ad50a4b1ef3120f5a6e7ce7b659e05
Hector Plahar <hplahar.jbei@gmail.com>
2012-03-31 13:32:40 -0700

    adding help text indicator; scroll when arrow buttons used to select cells out of range

--- src/main/java/org/jbei/ice/client/bulkimport/sheet/SheetHeader.java
+++ src/main/java/org/jbei/ice/client/bulkimport/sheet/SheetHeader.java
@@ -19,7 +19,12 @@ public class SheetHeader {
     }
 
     protected void addHeader(Header h) {
-        String html = h.toString();
+        String label = h.toString();
+        String top = label.length() > 15 ? "-2px" : "-8px";
+        String html = "<div style=\"position: relative; top: " + top
+                + "; left: -3px; float: right; font-size: 11px; "
+                + "border-radius: 1em 1em 1em 1em; color: #333; padding: 0 5px; "
+                + "font-weight: bold; background-color: #f8f8f0; cursor: pointer\">?</div>" + label;
         if (h.isRequired())
             html += (" <span class=\"required\">*</span>");
         HTML cell = new HTML(html);
