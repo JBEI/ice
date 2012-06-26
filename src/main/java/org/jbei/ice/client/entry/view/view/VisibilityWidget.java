b8c09e398d7d2ae629a125f67a370b36f2e30fc8
Hector Plahar <hplahar.jbei@gmail.com>
2012-06-25 17:07:24 -0700

    moving styles to .css

--- src/main/java/org/jbei/ice/client/entry/view/view/VisibilityWidget.java
+++ src/main/java/org/jbei/ice/client/entry/view/view/VisibilityWidget.java
@@ -4,7 +4,6 @@ import com.google.gwt.event.dom.client.MouseOverHandler;
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
 
 import static org.jbei.ice.client.entry.view.view.VisibilityWidgetPresenter.IVisibilityView;
@@ -15,16 +14,15 @@ import static org.jbei.ice.client.entry.view.view.VisibilityWidgetPresenter.IVis
 public class VisibilityWidget extends Composite implements IVisibilityView {
 
     private final FlexTable layout;
-    private final HTML tooltip;
+    private final Label tooltip;
     private final Label label;
     private final VisibilityWidgetPresenter presenter;
     private HandlerRegistration registration;
 
     public VisibilityWidget() {
 
-        tooltip = new HTML("<span style=\"position: relative; top: -2px; left: -3px; float: right; font-size: 11px; "
-                                   + "border-radius: 1em 1em 1em 1em; color: #333; padding: 0 5px; "
-                                   + "font-weight: bold; background-color: #f8f8f0; cursor: pointer\">?</span>");
+        tooltip = new Label("?");
+        tooltip.setStyleName("entry_pending_visibility_tooltip");
         label = new Label();
 
         layout = new FlexTable();
