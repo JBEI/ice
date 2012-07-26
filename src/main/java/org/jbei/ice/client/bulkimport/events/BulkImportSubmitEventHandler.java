13880f19995675e82767dc45fd3a9eb3c263ab8c
Hector Plahar <haplahar@lbl.gov>
2012-07-26 11:58:41 -0700

    refactoring (bulkimport->bulkupload)

--- src/main/java/org/jbei/ice/client/bulkimport/events/BulkImportSubmitEventHandler.java
+++
@@ -1,7 +0,0 @@
-package org.jbei.ice.client.bulkimport.events;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BulkImportSubmitEventHandler extends EventHandler {
-    void onSubmit(BulkImportSubmitEvent event);
-}
