014ba90043c5e42ef3083200d4ba840d9d87ade8
Hector Plahar <haplahar@lbl.gov>
2012-08-24 15:30:38 -0700

    fix for firefox to set focus on fileinput

--- src/main/java/org/jbei/ice/client/bulkupload/sheet/Sheet.java
+++ src/main/java/org/jbei/ice/client/bulkupload/sheet/Sheet.java
@@ -24,6 +24,7 @@ import com.google.gwt.user.client.DOM;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.FocusPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLTable.Cell;
 import com.google.gwt.user.client.ui.HasAlignment;
@@ -488,6 +489,9 @@ public class Sheet extends Composite implements SheetPresenter.View {
             }
             widget = newCellSelection.getWidget(newRow, true, tabIndex);
             sheetTable.setWidget(newRow, newCol, widget);
+            if (widget instanceof FocusPanel) {
+                ((FocusPanel) widget).setFocus(true);
+            }
         } else {
             widget = sheetTable.getWidget(newRow, newCol);
             if (widget instanceof CellWidget) {
