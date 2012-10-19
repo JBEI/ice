b1672bd6e91000d3f40d7a462b283eec9192cb20
Hector Plahar <haplahar@lbl.gov>
2012-10-19 11:20:03 -0700

    issue 32: permissions not being retrieved correctly

--- src/main/java/org/jbei/ice/lib/permissions/PermissionDAO.java
+++ src/main/java/org/jbei/ice/lib/permissions/PermissionDAO.java
@@ -3,16 +3,13 @@ package org.jbei.ice.lib.permissions;
 import org.jbei.ice.lib.account.model.Account;
 import org.jbei.ice.lib.dao.DAOException;
 import org.jbei.ice.lib.entry.model.Entry;
-import org.jbei.ice.lib.logging.Logger;
 import org.jbei.ice.lib.permissions.model.ReadUser;
 import org.jbei.ice.lib.permissions.model.WriteUser;
 import org.jbei.ice.server.dao.hibernate.HibernateRepository;
 
 import org.hibernate.Criteria;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
-import org.hibernate.criterion.Example;
-import org.hibernate.criterion.Projections;
+import org.hibernate.criterion.Restrictions;
 
 /**
  * @author Hector Plahar
@@ -26,27 +23,22 @@ public class PermissionDAO extends HibernateRepository {
      * @return True if given Account has read permission to the given Entry.
      */
     public boolean isReadUserAccount(Account account, Entry entry) throws DAOException {
-        Example example = Example.create(new ReadUser(entry, account));
-        return createCriteriaQuery(ReadUser.class, example);
+        Session session = newSession();
+        Criteria criteria = session.createCriteria(ReadUser.class)
+                                   .add(Restrictions.eq("account", account))
+                                   .add(Restrictions.eq("entry", entry));
+
+        Object result = criteria.uniqueResult();
+        return result != null;
     }
 
     public boolean isWriteUserAccount(Account account, Entry entry) throws DAOException {
-        Example example = Example.create(new WriteUser(entry, account));
-        return createCriteriaQuery(WriteUser.class, example);
-    }
-
-    protected boolean createCriteriaQuery(Class<?> c, Example example) throws DAOException {
         Session session = newSession();
+        Criteria criteria = session.createCriteria(WriteUser.class)
+                                   .add(Restrictions.eq("account", account))
+                                   .add(Restrictions.eq("entry", entry));
 
-        try {
-            Criteria criteria = session.createCriteria(c)
-                                       .add(example)
-                                       .setProjection(Projections.rowCount());
-            Integer integer = (Integer) criteria.uniqueResult();
-            return integer == 1;
-        } catch (HibernateException he) {
-            Logger.error(he);
-            throw new DAOException(he);
-        }
+        Object result = criteria.uniqueResult();
+        return result != null;
     }
 }
