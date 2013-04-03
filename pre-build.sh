0af10c158a013f97d254e553dc7fc15813c99c87
Hector Plahar <haplahar@lbl.gov>
2013-04-03 11:45:33 -0700

    - removing unused libsbolj and bytecode jars

--- pre-build.sh
+++ pre-build.sh
@@ -1,5 +1,4 @@
 cd lib
-mvn install:install-file -Dfile=bytecode.jar -DgroupId=org.jbei.ice -DartifactId=bytecode -Dversion=SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=biojava-1.7-all.jar -DgroupId=org.jbei.ice -DartifactId=biojava-all -Dversion=SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=biojava.jar -DgroupId=org.jbei.ice -DartifactId=biojava -Dversion=SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=backport-util-concurrent.jar -DgroupId=org.jbei.ice -DartifactId=backport-util-concurrent -Dversion=SNAPSHOT -Dpackaging=jar
