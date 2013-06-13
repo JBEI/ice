7bfb4218f6b7345f793d16bb7e97557eaa194ba8
Hector Plahar <haplahar@lbl.gov>
2013-06-13 13:15:56 -0700

    - removing share option from system collection sub menu

--- src/main/java/org/jbei/ice/client/collection/menu/SystemCollectionMenu.java
+++ src/main/java/org/jbei/ice/client/collection/menu/SystemCollectionMenu.java
@@ -1,9 +1,9 @@
 package org.jbei.ice.client.collection.menu;
 
+import org.jbei.ice.client.ClientController;
+
 import java.util.Arrays;
 import java.util.List;
-
-import org.jbei.ice.client.ClientController;
 
 /**
  * @author Hector Plahar
@@ -15,7 +15,7 @@ public class SystemCollectionMenu extends CollectionMenu {
         setEmptyCollectionMessage("No collections available");
 
         if (ClientController.account.isAdmin()) {
-            List<HoverOption> options = Arrays.asList(HoverOption.UNPIN, HoverOption.SHARE);
+            List<HoverOption> options = Arrays.asList(HoverOption.UNPIN);
             setCellHoverOptions(options);
         }
     }
