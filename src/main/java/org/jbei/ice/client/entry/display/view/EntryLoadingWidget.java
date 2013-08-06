3a1248ea10e67f564e21362cb4c97d6af3f13833
Hector Plahar <haplahar@lbl.gov>
2013-08-06 13:08:36 -0700

    - style changes for alert due to failed part retrieve

--- src/main/java/org/jbei/ice/client/entry/display/view/EntryLoadingWidget.java
+++ src/main/java/org/jbei/ice/client/entry/display/view/EntryLoadingWidget.java
@@ -1,5 +1,7 @@
 package org.jbei.ice.client.entry.display.view;
 
+import org.jbei.ice.client.common.widget.FAIconType;
+
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HasAlignment;
@@ -16,7 +18,6 @@ public class EntryLoadingWidget extends Composite {
 
     public EntryLoadingWidget() {
         panel = new FlexTable();
-        panel.setStyleName("entry_loading_indicator");
         panel.setWidth("100%");
         initWidget(panel);
     }
@@ -27,16 +28,19 @@ public class EntryLoadingWidget extends Composite {
             panel.setHeight(widget.getOffsetHeight() + "px");
         else
             panel.setHeight("450px");
-        panel.setHTML(0, 0, "<i class=\"icon-spinner icon-spin icon-3x\"></i><br><h2>LOADING CONTENT</h2>");
+        panel.setStyleName("entry_loading_indicator");
+        panel.setHTML(0, 0, "<i class=\"icon-spinner icon-spin icon-3x\"></i><br><h1>LOADING CONTENT</h1>");
         panel.getFlexCellFormatter().setAlignment(0, 0, HasAlignment.ALIGN_CENTER, HasAlignment.ALIGN_MIDDLE);
     }
 
     public void showErrorLoad() {
         panel.setHeight("300px");
+        panel.setStyleName("alert_indicator");
         panel.setHTML(0, 0, "<span style=\"line-height: 1px\">"
-                + "<i class=\"icon-ban-circle\" style=\"font-size: 9em; color: darkred\"></i>"
+                + "<i class=\"" + FAIconType.WARNING_SIGN.getStyleName()
+                + "\" style=\"font-size: 9em; color: darkred\"></i>"
                 + "<br><h2>COULD NOT LOAD CONTENT</h2>"
-                + "<h5>THIS IS LIKELY DUE TO INSUFFICIENT ACCESS PRIVILEGES OR ENTRY DOES NOT EXIST</h5></span>");
+                + "<h5>THIS IS LIKELY DUE TO INSUFFICIENT ACCESS PRIVILEGES OR THE PART DOES NOT EXIST</h5></span>");
         panel.getFlexCellFormatter().setAlignment(0, 0, HasAlignment.ALIGN_CENTER, HasAlignment.ALIGN_MIDDLE);
     }
 }
