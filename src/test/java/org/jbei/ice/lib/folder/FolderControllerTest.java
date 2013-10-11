3cc5f3bd1d7835b23cb7d814b632ec98c69327ac
Hector Plahar <haplahar@lbl.gov>
2013-10-11 08:59:46 -0700

    - updating folder content retrieve test to check for duplicates

--- src/test/java/org/jbei/ice/lib/folder/FolderControllerTest.java
+++ src/test/java/org/jbei/ice/lib/folder/FolderControllerTest.java
@@ -2,6 +2,7 @@ package org.jbei.ice.lib.folder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 import org.jbei.ice.lib.AccountCreator;
 import org.jbei.ice.lib.EntryCreator;
@@ -50,47 +51,45 @@ public class FolderControllerTest {
         // create folder
         FolderDetails folder = controller.createNewFolder(account, "test", "testing folder creation", null);
         Assert.assertNotNull(folder);
+        final short size = 105;
 
-        // create test strains
+        // create 100 test strains
         HashMap<String, Entry> parts = new HashMap<>();
-        for (int i = 0; i < 15; i += 1) {
+        for (int i = 0; i < size; i += 1) {
             Strain strain = EntryCreator.createTestStrain(account);
             Assert.assertNotNull(strain);
             parts.put(strain.getPartNumber(), strain);
         }
-        Assert.assertEquals(15, parts.size());
+        Assert.assertEquals(size, parts.size());
 
         // add to folder
         Folder added = controller.addFolderContents(account, folder.getId(), new ArrayList<Entry>(parts.values()));
         Assert.assertNotNull(added);
 
+        // keep track to find duplicates
+        HashSet<Long> set = new HashSet<>();
+
         // retrieve (supported sort types created, status, name, part_id, type)
         FolderDetails details = controller.retrieveFolderContents(account, folder.getId(), ColumnField.PART_ID, false,
-                                                                  0, 5);
+                                                                  0, 15);
         Assert.assertNotNull(details);
-        Assert.assertEquals(5, details.getEntries().size());
-        for (PartData partData : details.getEntries()) {
-            Assert.assertNotNull(parts.remove(partData.getPartId()));
-        }
-        Assert.assertEquals(10, parts.size());
 
-        // page 2
-        details = controller.retrieveFolderContents(account, folder.getId(), ColumnField.PART_ID, false, 5, 5);
-        Assert.assertNotNull(details);
-        Assert.assertEquals(5, details.getEntries().size());
-        for (PartData partData : details.getEntries()) {
-            Assert.assertNotNull(parts.remove(partData.getPartId()));
-        }
-        Assert.assertEquals(5, parts.size());
+        short pageSize = 15;
 
-        // page 3
-        details = controller.retrieveFolderContents(account, folder.getId(), ColumnField.PART_ID, false, 10, 5);
-        Assert.assertNotNull(details);
-        Assert.assertEquals(5, details.getEntries().size());
-        for (PartData partData : details.getEntries()) {
-            Assert.assertNotNull(parts.remove(partData.getPartId()));
+        int it = 1;
+        while (!details.getEntries().isEmpty()) {
+            Assert.assertEquals(pageSize, details.getEntries().size());
+            for (PartData partData : details.getEntries()) {
+                Assert.assertNotNull(parts.remove(partData.getPartId()));
+                Assert.assertFalse(set.contains(partData.getId()));
+                set.add(partData.getId());
+            }
+            // check remaining
+            Assert.assertEquals((size - (it * pageSize)), parts.size());
+            details = controller.retrieveFolderContents(account, folder.getId(), ColumnField.PART_ID, false,
+                                                        pageSize * it, pageSize);
+            it += 1;
         }
-        Assert.assertEquals(0, parts.size());
     }
 
     @After
