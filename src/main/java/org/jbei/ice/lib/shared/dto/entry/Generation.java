179bf0790e9855d6864aadfede23e60a02da540f
Hector Plahar <haplahar@lbl.gov>
2014-01-27 08:22:26 -0800

    adding F1-F3 to arabidopsis seed generation options

--- src/main/java/org/jbei/ice/lib/shared/dto/entry/Generation.java
+++ src/main/java/org/jbei/ice/lib/shared/dto/entry/Generation.java
@@ -10,7 +10,7 @@ import org.jbei.ice.lib.shared.dto.IDTOModel;
  * @author Hector Plahar
  */
 public enum Generation implements IDTOModel {
-    UNKNOWN, M0, M1, M2, T0, T1, T2, T3, T4, T5;
+    UNKNOWN, F1, F2, F3, M0, M1, M2, T0, T1, T2, T3, T4, T5;
 
     public static ArrayList<String> getDisplayList() {
         ArrayList<String> list = new ArrayList<String>();
