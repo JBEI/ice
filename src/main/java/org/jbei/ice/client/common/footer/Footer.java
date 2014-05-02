0029698a02376208a698fe30db94a3160341bce8
Hector Plahar <haplahar@lbl.gov>
2014-05-02 12:51:19 -0700

    increment version

--- src/main/java/org/jbei/ice/client/common/footer/Footer.java
+++ src/main/java/org/jbei/ice/client/common/footer/Footer.java
@@ -72,7 +72,7 @@ public class Footer extends Composite {
         line3.add(new HTML("&nbsp; | &nbsp;"));
         Anchor api = new Anchor("API", "/api");
         line3.add(api);
-        line3.add(new HTML("&nbsp; | &nbsp; v3.7.5"));
+        line3.add(new HTML("&nbsp; | &nbsp; v3.7.6"));
 
         panel.add(line3);
         return panel;
