a055dea4733cb08b2d54899ae625a79a1885365c
Hector <hector@Asias-MacBook-Pro.local>
2014-06-11 22:54:06 -0400

    local authentication

--- src/main/java/org/jbei/ice/lib/account/authentication/LocalAuthentication.java
+++ src/main/java/org/jbei/ice/lib/account/authentication/LocalAuthentication.java
@@ -1,5 +1,10 @@
 package org.jbei.ice.lib.account.authentication;
 
+import org.apache.commons.codec.binary.StringUtils;
+import org.jbei.ice.ControllerException;
+import org.jbei.ice.lib.account.AccountController;
+import org.jbei.ice.lib.account.model.Account;
+
 /**
  * Backend for authentication using the database. This is the default backend.
  *
@@ -12,14 +17,21 @@ public class LocalAuthentication implements IAuthentication {
 
     @Override
     public boolean authenticates(String userId, String password) throws AuthenticationException {
-        return false;
-//        AccountRetriever retriever = new AccountRetriever();
-//        boolean accountExists = retriever.accountExists(userId);
-//
-//        if (!accountExists)
-//            throw new AuthenticationException("Cannot authenticate against a non-existent account");
-//
-//        Authenticator authenticator = new Authenticator();
-//        return authenticator.isValidPassword(userId, password);
+        if(userId == null || password == null)
+            throw new AuthenticationException("Invalid username and password");
+
+        Account account;
+        AccountController controller = new AccountController();
+
+        try {
+            account = controller.getByEmail(userId);
+            if ((account == null) || (!controller.isValidPassword(account, password))) {
+                throw new AuthenticationException("Invalid Username or Password!");
+            }
+        } catch (ControllerException e) {
+            throw new AuthenticationException("Exception validating credentials", e);
+        }
+
+        return true;
     }
 }
