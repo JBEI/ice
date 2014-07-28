78a43f0b064ca859c1f6958cc1b0f3ac1b254300
Hector Plahar <haplahar@lbl.gov>
2014-07-28 12:23:37 -0700

    bug fix in entry to csv serialization

--- src/main/java/org/jbei/ice/lib/utils/IceCSVSerializer.java
+++ src/main/java/org/jbei/ice/lib/utils/IceCSVSerializer.java
@@ -1,7 +1,5 @@
 package org.jbei.ice.lib.utils;
 
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
 import java.util.List;
 
 import org.jbei.ice.lib.bulkupload.BulkCSVUploadHeaders;
@@ -33,16 +31,6 @@ public class IceCSVSerializer {
         return "";
     }
 
-    private static HashSet<String> getHeaders(EntryType type) {
-        HashSet<String> headers = new HashSet<>();
-        List<EntryField> fields = BulkCSVUploadHeaders.getHeadersForType(type);
-        for (EntryField field : fields) {
-            headers.add(field.getLabel());
-        }
-
-        return headers;
-    }
-
     public static String serialize(Entry entry) {
         final EntryType type = EntryType.nameToType(entry.getRecordType());
         List<EntryField> fields = BulkCSVUploadHeaders.getHeadersForType(type);
@@ -56,31 +44,8 @@ public class IceCSVSerializer {
         stringBuilder.append("Has Samples").append(",");
         stringBuilder.append("Has Sequence").append("\n");
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d, yyyy");
         SampleController sampleController = new SampleController();
         AttachmentController attachmentController = new AttachmentController();
-
-        stringBuilder.append(escapeCSVValue(entry.getRecordType())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getPartNumber())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getName())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getOwner())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getCreator())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getAlias())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getKeywords())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getSelectionMarkersAsString())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getLinksAsString())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getStatus())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getShortDescription())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getLongDescription())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getReferences())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getBioSafetyLevel())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getIntellectualProperty())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getPrincipalInvestigator())).append(",");
-        stringBuilder.append(escapeCSVValue(entry.getFundingSource())).append(",");
-        String time = (entry.getCreationTime() == null) ? "" : dateFormat.format(entry.getCreationTime());
-        stringBuilder.append(escapeCSVValue(time)).append(",");
-        time = (entry.getModificationTime() == null) ? "" : dateFormat.format(entry.getModificationTime());
-        stringBuilder.append(escapeCSVValue(time)).append(",");
 
         for (EntryField field : fields) {
             String value = EntryUtil.entryFieldToValue(entry, field);
