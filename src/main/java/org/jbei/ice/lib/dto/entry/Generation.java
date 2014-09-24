e0a1ab311e56ac29acf770ea0c87d3edaa0550b9
Hector Plahar <haplahar@lbl.gov>
2014-09-24 11:42:10 -0700

    adding F1-F3 generation options

--- src/main/java/org/jbei/ice/lib/dto/entry/Generation.java
+++ src/main/java/org/jbei/ice/lib/dto/entry/Generation.java
@@ -1,24 +1,15 @@
 package org.jbei.ice.lib.dto.entry;
 
-import java.util.ArrayList;
-
 import org.jbei.ice.lib.dao.IDataTransferModel;
 
 /**
- * Arabidopsis seed generation
+ * Arabidopsis seed generation options
  *
  * @author Hector Plahar
  */
 public enum Generation implements IDataTransferModel {
-    UNKNOWN, M0, M1, M2, T0, T1, T2, T3, T4, T5;
 
-    public static ArrayList<String> getDisplayList() {
-        ArrayList<String> list = new ArrayList<String>();
-        for (Generation option : Generation.values()) {
-            list.add(option.name());
-        }
-        return list;
-    }
+    UNKNOWN, F1, F2, F3, M0, M1, M2, T0, T1, T2, T3, T4, T5;
 
     public static Generation fromString(String value) {
         for (Generation option : Generation.values()) {
