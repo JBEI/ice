a39aa3b5edda5c9e852ec3ac3b12bdc35100bfba
Hector Plahar <haplahar@lbl.gov>
2014-11-17 18:07:12 -0800

    bug fix in localauthentication

--- src/main/java/org/jbei/ice/lib/account/authentication/LocalAuthentication.java
+++ src/main/java/org/jbei/ice/lib/account/authentication/LocalAuthentication.java
@@ -1,14 +1,14 @@
 package org.jbei.ice.lib.account.authentication;
 
-import org.jbei.ice.ControllerException;
 import org.jbei.ice.lib.account.AccountController;
 import org.jbei.ice.lib.account.AccountUtils;
 import org.jbei.ice.lib.account.model.Account;
 import org.jbei.ice.lib.common.logging.Logger;
 import org.jbei.ice.lib.dao.DAOFactory;
+import org.jbei.ice.lib.dto.ConfigurationKey;
 
 /**
- * Backend for authentication using the database. This is the default backend.
+ * Default ICE authentication scheme
  *
  * @author Hector Plahar
  */
@@ -22,46 +22,49 @@ public class LocalAuthentication implements IAuthentication {
         if (userId == null || password == null)
             throw new AuthenticationException("Invalid username and password");
 
-        Account account;
         AccountController controller = new AccountController();
-
-        try {
-            account = controller.getByEmail(userId);
-            if (account == null || !isValidPassword(account, password))
-                return null;
-            return account.getEmail();
-        } catch (ControllerException e) {
-            throw new AuthenticationException("Exception validating credentials", e);
-        }
+        Account account = controller.getByEmail(userId);
+        if (account == null || !isValidPassword(account, password))
+            return null;
+        return account.getEmail();
     }
 
     /**
-     * Check if the given password is valid for the account.
+     * Check if the given password is valid for the account. There are multiple checks for backward compatibility
+     * reasons
      *
-     * @param account
-     * @param password
-     * @return True if correct password.
-     * @throws ControllerException
+     * @param account  user account whose password is being checked
+     * @param password user entered password being checked for validation
+     * @return True if entered password matches one of encrypted schemes, false otherwise.
      */
-    protected boolean isValidPassword(Account account, String password) throws ControllerException {
+    protected boolean isValidPassword(Account account, String password) {
         if (account == null) {
-            throw new ControllerException("Failed to verify password for null Account!");
+            return false;
         }
+
+        // first check using the stronger encryption scheme
         boolean valid = account.getPassword().equals(AccountUtils.encryptNewUserPassword(password, account.getSalt()));
         if (valid)
             return valid;
-        String encrypted = AccountUtils.encryptPassword(password, account.getSalt());
-        valid = account.getPassword().equals(encrypted);
-        if (valid) {
-            // update
-            account.setPassword(encrypted);
-            try {
-                DAOFactory.getAccountDAO().update(account);
-            } catch (Exception e) {
-                Logger.error(e);
-            }
-            return true;
+
+        // invalid check for deprecated salt using older encryption scheme
+        String salt = ConfigurationKey.SECRET_KEY.getDefaultValue();
+        valid = account.getPassword().equals(AccountUtils.encryptPassword(password, salt));
+        if (!valid) {
+            // check old encryption scheme using user salt
+            valid = account.getPassword().equals(AccountUtils.encryptPassword(password, account.getSalt()));
+            if (!valid)
+                return false;
         }
-        return false;
+
+        // at this stage then password is valid, upgrade to new version
+        String newEncrypted = AccountUtils.encryptNewUserPassword(password, account.getSalt());
+        account.setPassword(newEncrypted);
+        try {
+            DAOFactory.getAccountDAO().update(account);
+        } catch (Exception e) {
+            Logger.error(e);
+        }
+        return true;
     }
 }
