d999632ff64dab05e690ee6e50374d56f7167150
Hector Plahar <haplahar@lbl.gov>
2014-12-16 17:23:31 -0800

    make search case insensitive

--- src/main/java/org/jbei/ice/lib/search/SearchController.java
+++ src/main/java/org/jbei/ice/lib/search/SearchController.java
@@ -112,12 +112,17 @@ public class SearchController {
     /**
      * Executes search using parameters specified in the query.
      *
-     * @param userId
-     * @param query
+     * @param userId unique user identifier making the request. This can be null if the request is via web of
+     *               registries
+     * @param query  wrapper around search query
      * @return wrapper around the list of search results
      */
     public SearchResults runLocalSearch(String userId, SearchQuery query) {
-        String queryString = query.getQueryString().toLowerCase();
+        String queryString;
+        if (query.getQueryString() != null)
+            queryString = query.getQueryString().toLowerCase();
+        else
+            queryString = query.getQueryString();
         Account account = null;
         if (userId != null)
             account = DAOFactory.getAccountDAO().getByEmail(userId);
