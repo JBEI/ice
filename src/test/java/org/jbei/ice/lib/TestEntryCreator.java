15e5106b98254bd33a8606ee4329f047f4858b57
Hector Plahar <haplahar@lbl.gov>
2015-01-12 10:13:16 -0800

    update test

--- src/test/java/org/jbei/ice/lib/TestEntryCreator.java
+++ src/test/java/org/jbei/ice/lib/TestEntryCreator.java
@@ -3,6 +3,7 @@ package org.jbei.ice.lib;
 import org.jbei.ice.lib.account.model.Account;
 import org.jbei.ice.lib.entry.EntryCreator;
 import org.jbei.ice.lib.entry.model.Strain;
+import org.jbei.ice.lib.shared.BioSafetyOption;
 
 /**
  * Helper class for creating entries to be used in test.
@@ -15,6 +16,16 @@ public class TestEntryCreator {
     public static Strain createTestStrain(Account account) throws Exception {
         Strain strain = new Strain();
         strain.setName("sTrain");
+        strain = (Strain) new EntryCreator().createEntry(account, strain, null);
+        return strain;
+    }
+
+    public static Strain createTestAccountAndStrain(String userId) throws Exception {
+        Account account = AccountCreator.createTestAccount(userId, false);
+        Strain strain = new Strain();
+        strain.setName("sTrain");
+        strain.setBioSafetyLevel(BioSafetyOption.LEVEL_ONE.ordinal());
+        strain.setShortDescription("test strain");
         strain = (Strain) new EntryCreator().createEntry(account, strain, null);
         return strain;
     }
