a6c6dc9aa7e5762c1379b98932dd8d09450c7509
William Morrell <wcmorrell@lbl.gov>
2015-04-02 19:04:44 -0700

    Converted remaining resources

--- src/main/java/org/jbei/ice/services/rest/RemoteAccessResource.java
+++ src/main/java/org/jbei/ice/services/rest/RemoteAccessResource.java
@@ -1,9 +1,9 @@
 package org.jbei.ice.services.rest;
 
 import java.util.ArrayList;
+
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
-import javax.ws.rs.HeaderParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -20,9 +20,8 @@ import org.jbei.ice.lib.net.RemoteAccessController;
 import org.jbei.ice.lib.vo.FeaturedDNASequence;
 
 /**
- * REST resource for sending/retrieving messages from remote
- * ICE instances. Local instances access this resource which contacts the remote
- * resource on its behalf
+ * REST resource for sending/retrieving messages from remote ICE instances. Local instances access
+ * this resource which contacts the remote resource on its behalf
  *
  * @author Hector Plahar
  */
@@ -32,77 +31,111 @@ public class RemoteAccessResource extends RestResource {
     private RemoteAccessController controller = new RemoteAccessController();
 
     /**
-     * @param remoteId unique identifier for remote partner being accessed
+     * @param remoteId
+     *            unique identifier for remote partner being accessed
      * @return list of available folders that are available on the registry
      */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/available")
-    public ArrayList<FolderDetails> readRemoteUser(@PathParam("id") long remoteId) {
+    public ArrayList<FolderDetails> readRemoteUser(@PathParam("id") final long remoteId) {
         return controller.getAvailableFolders(remoteId);
     }
 
+    /**
+     * @param remoteId
+     * @param email
+     * @return user from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/users/{email}")
-    public AccountTransfer getRemoteUser(@PathParam("id") long remoteId,
-            @PathParam("email") String email) {
+    public AccountTransfer getRemoteUser(@PathParam("id") final long remoteId,
+            @PathParam("email") final String email) {
         return controller.getRemoteUser(remoteId, email);
     }
 
+    /**
+     * @param remoteId
+     * @param partId
+     * @return sequence from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/{entryId}/sequence")
-    public Response getSequence(@PathParam("id") long remoteId,
-            @PathParam("entryId") long partId) {
-        FeaturedDNASequence sequence = controller.getRemoteSequence(remoteId, partId);
-        if (sequence == null)
+    public Response getSequence(@PathParam("id") final long remoteId,
+            @PathParam("entryId") final long partId) {
+        final FeaturedDNASequence sequence = controller.getRemoteSequence(remoteId, partId);
+        if (sequence == null) {
             return Response.status(Response.Status.NO_CONTENT).build();
+        }
         return Response.status(Response.Status.OK).entity(sequence).build();
     }
 
+    /**
+     * @param remoteId
+     * @param partId
+     * @return traces from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/parts/{entryId}/traces")
-    public Response getSequenceTraces(@PathParam("id") long remoteId,
-            @PathParam("entryId") long partId) {
-        ArrayList<TraceSequenceAnalysis> traces = controller.getRemoteTraces(remoteId, partId);
-        if (traces == null)
+    public Response getSequenceTraces(@PathParam("id") final long remoteId,
+            @PathParam("entryId") final long partId) {
+        final ArrayList<TraceSequenceAnalysis> traces = controller
+                .getRemoteTraces(remoteId, partId);
+        if (traces == null) {
             return Response.status(Response.Status.NO_CONTENT).build();
+        }
         return Response.status(Response.Status.OK).entity(traces).build();
     }
 
+    /**
+     * @param remoteId
+     * @param folderId
+     * @param offset
+     * @param limit
+     * @param sort
+     * @param asc
+     * @return public folders from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/folders/{folderId}")
-    public FolderDetails getPublicFolderEntries(
-            @PathParam("id") long remoteId,
-            @PathParam("folderId") long folderId,
-            @DefaultValue("0") @QueryParam("offset") int offset,
-            @DefaultValue("15") @QueryParam("limit") int limit,
-            @DefaultValue("created") @QueryParam("sort") String sort,
-            @DefaultValue("false") @QueryParam("asc") boolean asc,
-            @HeaderParam(value = "X-ICE-Authentication-SessionId") String userAgentHeader) {
+    public FolderDetails getPublicFolderEntries(@PathParam("id") final long remoteId,
+            @PathParam("folderId") final long folderId,
+            @DefaultValue("0") @QueryParam("offset") final int offset,
+            @DefaultValue("15") @QueryParam("limit") final int limit,
+            @DefaultValue("created") @QueryParam("sort") final String sort,
+            @DefaultValue("false") @QueryParam("asc") final boolean asc) {
         return controller.getPublicFolderEntries(remoteId, folderId, sort, asc, offset, limit);
     }
 
+    /**
+     * @param remoteId
+     * @param partId
+     * @return part samples from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/parts/{partId}/samples")
-    public Response getRemotePartSamples(@PathParam("id") long remoteId,
-            @PathParam("partId") long partId,
-            @HeaderParam(value = "X-ICE-Authentication-SessionId") String userAgentHeader) {
-        ArrayList<PartSample> result = controller.getRemotePartSamples(remoteId, partId);
+    public Response getRemotePartSamples(@PathParam("id") final long remoteId,
+            @PathParam("partId") final long partId) {
+        final ArrayList<PartSample> result = controller.getRemotePartSamples(remoteId, partId);
         return super.respond(result);
     }
 
+    /**
+     * @param remoteId
+     * @param partId
+     * @return comments from remote ICE
+     */
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/parts/{partId}/comments")
-    public Response getRemotePartComments(@PathParam("id") long remoteId,
-            @PathParam("partId") long partId,
-            @HeaderParam(value = "X-ICE-Authentication-SessionId") String userAgentHeader) {
-        ArrayList<UserComment> result = controller.getRemotePartComments(remoteId, partId);
+    public Response getRemotePartComments(@PathParam("id") final long remoteId,
+            @PathParam("partId") final long partId) {
+        final ArrayList<UserComment> result = controller.getRemotePartComments(remoteId, partId);
         return super.respond(result);
     }
 }
