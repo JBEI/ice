e3d679d641f1d1e229a3bcfa7fe428051d38ae57
William Morrell <wcmorrell@lbl.gov>
2015-04-15 20:31:27 -0700

    Moved HMAC authentication to a request filter/interceptor to get access to request message body before conversion to POJO entity

--- src/main/java/org/jbei/ice/services/rest/RestResource.java
+++ src/main/java/org/jbei/ice/services/rest/RestResource.java
@@ -1,26 +1,14 @@
 package org.jbei.ice.services.rest;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Paths;
-import java.security.Key;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
-import org.jbei.auth.Authorization;
-import org.jbei.auth.KeyTable;
-import org.jbei.auth.hmac.HmacAuthorizor;
-import org.jbei.auth.hmac.HmacSignatureFactory;
+import org.jbei.auth.hmac.HmacSignature;
 import org.jbei.ice.lib.account.SessionHandler;
 import org.jbei.ice.lib.common.logging.Logger;
-import org.jbei.ice.lib.dao.hibernate.HibernateUtil;
-import org.jbei.ice.lib.dto.ConfigurationKey;
-import org.jbei.ice.lib.utils.Utils;
 
 /**
  * Parent class for all rest resource objects
@@ -28,45 +16,6 @@ import org.jbei.ice.lib.utils.Utils;
  * @author Hector Plahar
  */
 public class RestResource {
-
-    // do lookup by using existing configuration DATA_DIRECTORY to find key names => key data
-    private static final KeyTable TABLE = new KeyTable() {
-
-        // keys stored in /var/lib/tomcat6/data/rest-auth by default
-        private final File directory;
-        {
-            // need to force-create a transaction to get the DATA_DIRECTORY config value
-            HibernateUtil.beginTransaction();
-            directory = Paths.get(Utils.getConfigValue(ConfigurationKey.DATA_DIRECTORY),
-                    "rest-auth").toFile();
-            HibernateUtil.commitTransaction();
-        }
-
-        @Override
-        public Key getKey(final String keyId) {
-            try {
-                // find file named by keyId in the directory
-                final File keyFile = new File(directory, keyId);
-                // collect all lines in the file to a buffer
-                final StringBuilder encoded = new StringBuilder();
-                try (final FileReader reader = new FileReader(keyFile);
-                        final BufferedReader buffered = new BufferedReader(reader);) {
-                    String line;
-                    while ((line = buffered.readLine()) != null) {
-                        encoded.append(line);
-                    }
-                    // after reading all lines, decode value into a Key object
-                    return HmacSignatureFactory.decodeKey(encoded.toString());
-                }
-            } catch (final Throwable t) {
-                Logger.error("Failed to load rest-auth key " + keyId);
-            }
-            return null;
-        }
-
-    };
-
-    private static final HmacAuthorizor AUTHORIZOR = new HmacAuthorizor(TABLE);
 
     @HeaderParam(value = "X-ICE-Authentication-SessionId")
     private String sessionId;
@@ -87,21 +36,7 @@ public class RestResource {
      *             for unauthorized access
      */
     protected String getUserId() {
-        final Authorization auth;
-        if (userId != null) {
-            // we've already looked up the userId
-            return userId;
-        } else if ((auth = AUTHORIZOR.validate(request)).isValid()) {
-            userId = auth.getUserId();
-            // TODO validation of meaningful userId
-            // e.g. "admin" account on EDD won't mean anything to ICE
-        } else if (sessionId != null) {
-            userId = SessionHandler.getUserIdBySession(sessionId);
-        }
-        if (userId == null) {
-            throw new WebApplicationException(Response.Status.UNAUTHORIZED);
-        }
-        return userId;
+        return getUserId(sessionId);
     }
 
     /**
@@ -114,16 +49,22 @@ public class RestResource {
      *             for unauthorized access
      */
     protected String getUserId(final String sessionId) {
-        final Authorization auth;
         if (userId != null) {
             // we've already looked up the userId
             return userId;
         } else if ((userId = SessionHandler.getUserIdBySession(sessionId)) != null) {
             // try to get user from a session ID, continue to Authorization if fails
-        } else if ((auth = AUTHORIZOR.validate(request)).isValid()) {
-            userId = auth.getUserId();
-            // TODO validation of meaningful userId
-            // e.g. "admin" account on EDD won't mean anything to ICE
+        } else {
+            final Object hmac = request.getAttribute(AuthenticationInterceptor.HMAC_SIGNATURE);
+            final Object valid = request.getAttribute(AuthenticationInterceptor.EXPECTED_SIGNATURE);
+            if (hmac != null && hmac instanceof HmacSignature) {
+                final HmacSignature generated = (HmacSignature) hmac;
+                if (generated.generateSignature().equals(valid)) {
+                    // TODO validation of meaningful userId
+                    // e.g. "admin" account on EDD won't mean anything to ICE
+                    userId = generated.getUserId();
+                }
+            }
         }
         if (userId == null) {
             throw new WebApplicationException(Response.Status.UNAUTHORIZED);
