6fb9851fdcc5abbfdcea363b5f769ff2aa899752
Hector Plahar <HAPlahar@lbl.gov>
2015-08-12 10:38:19 -0700

    start of bulk upload with samples (currently file upload of seeds only)

--- src/main/java/org/jbei/ice/lib/bulkupload/HeaderValue.java
+++ src/main/java/org/jbei/ice/lib/bulkupload/HeaderValue.java
@@ -1,26 +1,12 @@
 package org.jbei.ice.lib.bulkupload;
 
-import org.jbei.ice.lib.dto.bulkupload.EntryField;
-
 /**
  * Value of header
  *
  * @author Hector Plahar
  */
-public class HeaderValue {
-    private boolean isSubType;
-    private EntryField entryField;
+public interface HeaderValue {
 
-    public HeaderValue(boolean subType, EntryField entryField) {
-        this.isSubType = subType;
-        this.entryField = entryField;
-    }
+    boolean isSampleField();
 
-    public boolean isSubType() {
-        return isSubType;
-    }
-
-    public EntryField getEntryField() {
-        return entryField;
-    }
 }
