70ab10382903a0e0729785e6945b76221fccbc2e
Hector Plahar <hector.plahar@gmail.com>
2015-11-10 10:00:41 -0800

    ui tweaks for height / spacing etc

--- src/main/java/org/jbei/ice/lib/shared/ColumnField.java
+++ src/main/java/org/jbei/ice/lib/shared/ColumnField.java
@@ -9,36 +9,26 @@ import org.jbei.ice.storage.IDataTransferModel;
  */
 public enum ColumnField implements IDataTransferModel {
 
-    SELECTION("Select"),
-    TYPE("Type"),
-    PART_ID("Part ID"),
-    CREATED("Created"),
-    NAME("Name"),
-    SUMMARY("Summary"),
-    REGISTRY_NAME("Registry Name"),
-    OWNER("Owner"),
-    STATUS("Status"),
-    BIT_SCORE("Bit Score"),
-    E_VALUE("E-Value"),
-    ALIGNED_BP("Aligned (BP)"),
-    ALIGNED_IDENTITY("Aligned % Identity"),
-    LAST_ADDED("Last Added"),
-    LAST_VISITED("Last Visited"),
-    LABEL("Label"),
-    NOTES("Notes"),
-    LOCATION("Location"),
-    DESCRIPTION("Description"),
-    COUNT("Member Count"),
-    RELEVANCE("Relevance"),
-    ALIGNMENT("Alignment");
-
-    private String name;
-
-    ColumnField(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
+    SELECTION,
+    TYPE,
+    PART_ID,
+    CREATED,
+    NAME,
+    SUMMARY,
+    REGISTRY_NAME,
+    OWNER,
+    STATUS,
+    BIT_SCORE,
+    E_VALUE,
+    ALIGNED_BP,
+    ALIGNED_IDENTITY,
+    LAST_ADDED,
+    LAST_VISITED,
+    LABEL,
+    NOTES,
+    LOCATION,
+    DESCRIPTION,
+    COUNT,
+    RELEVANCE,
+    ALIGNMENT
 }
