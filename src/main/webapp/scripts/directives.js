38d589be7547235a8bde47745f1806d9d711ff96
Hector Plahar <hector.plahar@gmail.com>
2015-11-11 13:48:24 -0800

    moving wor directive to wor package

--- src/main/webapp/scripts/directives.js
+++ src/main/webapp/scripts/directives.js
@@ -195,26 +195,6 @@ iceDirectives.directive("ice.menu.collections.details", function () {
     };
 });
 
-// web of registries menu directive
-iceDirectives.directive("ice.menu.wor", function () {
-    return {
-        restrict:"E",
-        templateUrl:"scripts/wor/web-of-registries-menu.html",
-        controller:"WebOfRegistriesMenuController"
-    }
-});
-
-iceDirectives.directive("ice.menu.wor.details", function () {
-    return {
-        restrict:"E", // match element name ("A" for attribute - e.g. <div ice.menu.collections></div>)
-        templateUrl:"scripts/wor/web-of-registries-menu-details.html",
-        controller:"WebOfRegistriesDetailController"
-//        link: function ( scope, element, attributes ){
-//            element.bind( "click", function)
-//        }
-    };
-});
-
 // tags menu directive
 iceDirectives.directive("ice.menu.tags", function () {
     return {
