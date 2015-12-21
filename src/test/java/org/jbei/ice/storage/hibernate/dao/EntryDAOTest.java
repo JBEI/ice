689a6efff9327410ea62e56a8abb58161c0f3565
Hector Plahar <hector.plahar@gmail.com>
2015-12-21 11:26:04 -0800

    unit tests

--- src/test/java/org/jbei/ice/storage/hibernate/dao/EntryDAOTest.java
+++ src/test/java/org/jbei/ice/storage/hibernate/dao/EntryDAOTest.java
@@ -1,10 +1,12 @@
 package org.jbei.ice.storage.hibernate.dao;
 
 import org.jbei.ice.lib.AccountCreator;
+import org.jbei.ice.lib.TestEntryCreator;
 import org.jbei.ice.lib.entry.EntryCreator;
 import org.jbei.ice.lib.shared.BioSafetyOption;
 import org.jbei.ice.storage.hibernate.HibernateUtil;
 import org.jbei.ice.storage.model.Account;
+import org.jbei.ice.storage.model.Entry;
 import org.jbei.ice.storage.model.SelectionMarker;
 import org.jbei.ice.storage.model.Strain;
 import org.junit.After;
@@ -36,6 +38,14 @@ public class EntryDAOTest {
     }
 
     @Test
+    public void testGetEntrySummary() throws Exception {
+        Account account = AccountCreator.createTestAccount("testGetEntrySummary", false);
+        long id = TestEntryCreator.createTestPart(account.getEmail());
+        String summary = entryDAO.getEntrySummary(id);
+        Assert.assertEquals("summary for test", summary);
+    }
+
+    @Test
     public void testGetMatchingSelectionMarkers() throws Exception {
         String email = "testGetMatchingSelectionMarkers";
         Account account = AccountCreator.createTestAccount(email, false);
@@ -46,7 +56,7 @@ public class EntryDAOTest {
         strain.setShortDescription("test strain");
 
         SelectionMarker marker = new SelectionMarker();
-        marker.setName("selection marker");
+        marker.setName("xkcd");
         SelectionMarker marker2 = new SelectionMarker();
         marker2.setName("test");
 
@@ -60,11 +70,53 @@ public class EntryDAOTest {
 
         Assert.assertEquals(2, strain.getSelectionMarkers().size());
 
-        List<String> results = entryDAO.getMatchingSelectionMarkers("mark", 5);
+        List<String> results = entryDAO.getMatchingSelectionMarkers("xkcd", 5);
         Assert.assertEquals(1, results.size());
 
         List<String> res = entryDAO.getMatchingSelectionMarkers("tes", 5);
         Assert.assertEquals(1, res.size());
         Assert.assertEquals("test", res.get(0));
+    }
+
+    @Test
+    public void testMatchingPlasmidField() throws Exception {
+        // create plasmids
+    }
+
+    @Test
+    public void testGet() throws Exception {
+        Account account = AccountCreator.createTestAccount("testGet", false);
+        long id = TestEntryCreator.createTestPart(account.getEmail());
+        Entry entry = entryDAO.get(id);
+        Assert.assertNotNull(entry);
+        Assert.assertEquals(id, entry.getId());
+    }
+
+    @Test
+    public void testGetByRecordId() throws Exception {
+        Account account = AccountCreator.createTestAccount("testGetByRecordId", false);
+        long id = TestEntryCreator.createTestPart(account.getEmail());
+        Entry entry = entryDAO.get(id);
+        Assert.assertNotNull(entry);
+        Entry rEntry = entryDAO.getByRecordId(entry.getRecordId());
+        Assert.assertNotNull(rEntry);
+        Assert.assertEquals(entry.getRecordId(), rEntry.getRecordId());
+    }
+
+    @Test
+    public void testGetByPartNumber() throws Exception {
+        Account account = AccountCreator.createTestAccount("testGetByPartNumber", false);
+        long id = TestEntryCreator.createTestPart(account.getEmail());
+        Entry entry = entryDAO.get(id);
+        Assert.assertNotNull(entry);
+        Entry result = entryDAO.getByPartNumber(entry.getPartNumber());
+        Assert.assertNotNull(result);
+        Assert.assertEquals(entry.getPartNumber(), result.getPartNumber());
+    }
+
+
+    @Test
+    public void testRetrieveVisibleEntries() throws Exception {
+
     }
 }
