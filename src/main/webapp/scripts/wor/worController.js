554e09bc3e45e1c3a3bf747e714d5ea089c3653a
Hector Plahar <hector.plahar@gmail.com>
2016-03-09 08:50:25 -0800

    sort for web of registries folders

--- src/main/webapp/scripts/wor/worController.js
+++ src/main/webapp/scripts/wor/worController.js
@@ -349,7 +349,8 @@ angular.module('ice.wor.controller', [])
         }
     })
     .controller('WebOfRegistriesMenuController', function ($rootScope, $scope, $location, $uibModal, $cookieStore,
-                                                           $stateParams, WebOfRegistries, Remote, Settings, Util) {
+                                                           $stateParams, WebOfRegistries, Remote, Settings, Util,
+                                                           localStorageService) {
         // retrieve web of registries partners
         $scope.wor = WebOfRegistries().query({approved_only: true});
         $scope.selectedPartner = $stateParams.partner;
@@ -359,6 +360,27 @@ angular.module('ice.wor.controller', [])
                 $scope.selectedPartnerFolders = result;
             });
         }
+
+        $scope.worMenuSortParams = localStorageService.get('worCollectionFolderSortParams');
+        if (!$scope.worMenuSortParams) {
+            $scope.worMenuSortParams = {field: 'creationTime', asc: true};
+        }
+
+        $scope.sortWorCollectionFolders = function () {
+            if ($scope.worMenuSortParams.field == 'creationTime') {
+                if (!$scope.worMenuSortParams.asc) {
+                    $scope.worMenuSortParams.field = 'folderName';
+                }
+                $scope.worMenuSortParams.asc = false;
+            } else {
+                // sort by name
+                if ($scope.worMenuSortParams.asc) {
+                    $scope.worMenuSortParams.field = 'creationTime';
+                }
+                $scope.worMenuSortParams.asc = true;
+            }
+            localStorageService.set('worCollectionFolderSortParams', $scope.worMenuSortParams);
+        };
 
         // retrieve web of registries setting
         var sessionId = $cookieStore.get("sessionId");
