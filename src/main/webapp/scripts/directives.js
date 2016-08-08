ce8b9d3f9b19c91bc6c7d33a5a925c740a6a5cde
Hector Plahar <hector.plahar@gmail.com>
2016-08-08 15:40:57 -0700

    cleanup

--- src/main/webapp/scripts/directives.js
+++ src/main/webapp/scripts/directives.js
@@ -207,51 +207,7 @@ iceDirectives.directive("ice.menu.tags", function () {
     }
 });
 
-iceDirectives.directive('lazyLoad', ['$window', '$q', function ($window, $q) {
-    function load_script() {
-        var s = document.createElement('script'); // use global document since Angular's $document is weak
-        s.src = 'https://maps.googleapis.com/maps/api/js?sensor=false&callback=initialize';
-        document.body.appendChild(s);
-    }
-
-    //function lazyLoadApi(key) {
-    //    var deferred = $q.defer();
-    //    $window.initialize = function () {
-    //        deferred.resolve();
-    //    };
-    //    // thanks to Emil Stenström: http://friendlybit.com/js/lazy-loading-asyncronous-javascript/
-    //    if ($window.attachEvent) {
-    //        $window.attachEvent('onload', load_script);
-    //    } else {
-    //        $window.addEventListener('load', load_script, false);
-    //    }
-    //    return deferred.promise;
-    //}
-    return {
-        restrict: 'E',
-        link: function (scope, element, attrs) { // function content is optional
-            // in this example, it shows how and when the promises are resolved
-            //if ($window.google && $window.google.maps) {
-            //    console.log('gmaps already loaded');
-            //} else {
-            //    //lazyLoadApi().then(function () {
-            //    //    console.log('promise resolved');
-            //    //    if ($window.google && $window.google.maps) {
-            //    //        console.log('gmaps loaded');
-            //    //    } else {
-            //    //        console.log('gmaps not loaded');
-            //    //    }
-            //    //}, function () {
-            //    //    console.log('promise rejected');
-            //    //});
-            //}
-            load_script();
-
-        }
-    };
-}]);
-
-iceDirectives.directive("iceVectorViewer", function ($cookieStore) {
+iceDirectives.directive("iceVectorViewer", function ($cookieStore, $location) {
     function link(scope, element, attrs) {
         var sid = $cookieStore.get("sessionId");
         var entryId;
