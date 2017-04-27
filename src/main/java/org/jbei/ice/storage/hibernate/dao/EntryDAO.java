25a0bd97dbb38e2db4f5374ad06ec0ac1602d2d6
Hector Plahar <haplahar@lbl.gov>
2017-04-27 10:31:31 -0700

    bug fix: retrieve distinct entries for featured collection

--- src/main/java/org/jbei/ice/storage/hibernate/dao/EntryDAO.java
+++ src/main/java/org/jbei/ice/storage/hibernate/dao/EntryDAO.java
@@ -164,7 +164,7 @@ public class EntryDAO extends HibernateRepository<Entry> {
     public List<Entry> retrieveVisibleEntries(Account account, Set<Group> groups, ColumnField sortField, boolean asc,
                                               int start, int count, String filter) {
         try {
-            CriteriaQuery<Entry> query = getBuilder().createQuery(Entry.class);
+            CriteriaQuery<Entry> query = getBuilder().createQuery(Entry.class).distinct(true);
             Root<Entry> from = query.from(Entry.class);
             Join<Entry, Permission> entryPermission = from.join("permissions");
 
