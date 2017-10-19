2cac106fe40c1ea70c405200089cdceccedc5165
Hector Plahar <haplahar@lbl.gov>
2017-10-19 15:51:02 -0700

    bug fix in truncate filter

--- src/main/webapp/scripts/filters.js
+++ src/main/webapp/scripts/filters.js
@@ -30,12 +30,10 @@ iceFilters.filter('truncate', function () {
         if (end === undefined)
             end = "...";
 
-        if (text.length <= length || text.length - end.length <= length) {
+        if (text.length <= length)
             return text;
-        }
-        else {
-            return String(text).substring(0, length - end.length) + end;
-        }
+
+        return String(text).substring(0, length - end.length) + end;
     };
 });
 
