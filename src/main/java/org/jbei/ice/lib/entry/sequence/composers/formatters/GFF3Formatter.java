8b6db4972c6e91883c44cc23a9a75faff76f8c83
Hector Plahar <hector.plahar@gmail.com>
2017-12-12 16:01:51 -0800

    synbio-1460: provide support for gff3 downloads

---
+++ src/main/java/org/jbei/ice/lib/entry/sequence/composers/formatters/GFF3Formatter.java
@@ -0,0 +1,46 @@
+package org.jbei.ice.lib.entry.sequence.composers.formatters;
+
+import org.jbei.ice.storage.model.AnnotationLocation;
+import org.jbei.ice.storage.model.Sequence;
+import org.jbei.ice.storage.model.SequenceFeature;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Set;
+
+/**
+ * Formatter for GFF3 Format
+ *
+ * @author Hector Plahar
+ */
+public class GFF3Formatter extends AbstractFormatter {
+
+    @Override
+    public void format(Sequence sequence, OutputStream outputStream) throws FormatterException, IOException {
+        if (sequence == null)
+            throw new IllegalArgumentException("Cannot write null sequence");
+
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("##gff-version 3").append(System.lineSeparator());
+        Set<SequenceFeature> featureSet = sequence.getSequenceFeatures();
+        if (featureSet != null) {
+            for (SequenceFeature sequenceFeature : featureSet) {
+                String featureLine = sequenceFeature.getName() + " ICE " + sequenceFeature.getGenbankType();
+
+                // location
+                for (AnnotationLocation location : sequenceFeature.getAnnotationLocations()) {
+                    builder.append(featureLine).append(" ")
+                            .append(location.getGenbankStart()).append(" ")
+                            .append(location.getEnd()).append(" ")
+                            .append(". ")
+                            .append(sequenceFeature.getStrand() == 1 ? "+ " : "- ")
+                            .append(". ");
+                    builder.append(System.lineSeparator());
+                }
+            }
+        }
+
+        outputStream.write(builder.toString().getBytes());
+    }
+}
