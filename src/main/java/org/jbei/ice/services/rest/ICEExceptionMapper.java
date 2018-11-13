e0f8f174758952254355f10c95d763dd396a3b24
wmorrell <wmorrell@users.noreply.github.com>
2018-11-12 16:14:10 -0800

    Change ExceptionMapper to not email on every 404 (#81)

--- src/main/java/org/jbei/ice/services/rest/ICEExceptionMapper.java
+++ src/main/java/org/jbei/ice/services/rest/ICEExceptionMapper.java
@@ -11,6 +11,7 @@ import javax.ws.rs.ext.Provider;
  * Exception mapper for mapping exceptions to {@link Response}s
  *
  * @author Hector Plahar
+ * @author William Morrell
  */
 @Provider
 public class ICEExceptionMapper implements ExceptionMapper<Exception> {
@@ -25,8 +26,33 @@ public class ICEExceptionMapper implements ExceptionMapper<Exception> {
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
 
-        if (response.getStatus() != Response.Status.UNAUTHORIZED.getStatusCode())
-            Logger.error(exception);
+        // Handle logging based on response HTTP codes
+        final int status = response.getStatus();
+        // find where the exception came from
+        final StackTraceElement[] traces = exception.getStackTrace();
+        final String where;
+        if (traces.length > 0) {
+        	where = traces[0].toString();
+        } else {
+        	where = "Unknown trace location";
+        }
+        final String info = "HTTP " + status + " thrown from " + where;
+        // anything 1xx or 2xx using an Exception is weird; log as WARNING
+        if (status < 300) {
+        	Logger.warn(info);
+        }
+        // log all 3xx HTTP errors as INFO
+        if (300 <= status && status < 400) {
+        	Logger.info(info);
+        }
+        // log all 4xx HTTP errors as WARNING
+        else if (400 <= status && status < 500) {
+        	Logger.warn(info);
+        }
+        // log all other HTTP errors as ERROR; generates email by default
+        else {
+            Logger.error(info, exception);
+        }
 
         return response;
     }
