14d96866f8f3f4ab87a3cbed63b6eb02bed73c87
Hector Plahar <haplahar@lbl.gov>
2018-12-18 10:14:05 -0800

    version 5.5.0

--- src/main/java/org/jbei/ice/lib/config/SiteSettings.java
+++ src/main/java/org/jbei/ice/lib/config/SiteSettings.java
@@ -9,7 +9,7 @@ import org.jbei.ice.storage.IDataTransferModel;
  */
 public class SiteSettings implements IDataTransferModel {
 
-    private String version = "5.4.18";
+    private String version = "5.5.0";
     private String assetName;
     private boolean hasLogo;
     private boolean hasLoginMessage;
