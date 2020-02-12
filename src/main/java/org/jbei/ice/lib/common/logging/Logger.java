53656a0cd621c4484afb1f69ebc0d2a2f0bfc41e
Hector Plahar <haplahar@lbl.gov>
2020-02-12 08:25:18 -0800

    clean up and simplify logger

--- src/main/java/org/jbei/ice/lib/common/logging/Logger.java
+++ src/main/java/org/jbei/ice/lib/common/logging/Logger.java
@@ -1,13 +1,6 @@
 package org.jbei.ice.lib.common.logging;
 
-import org.hibernate.HibernateException;
-import org.jbei.ice.lib.dto.ConfigurationKey;
-import org.jbei.ice.lib.email.EmailFactory;
-import org.jbei.ice.lib.utils.Utils;
 import org.slf4j.LoggerFactory;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Logger for ICE.
@@ -16,30 +9,18 @@ import java.util.Date;
  */
 public class Logger {
 
-    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger("org.jbei.ice");
+    private static final ch.qos.logback.classic.Logger LOGGER = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("org.jbei.ice");
 
     public static void error(String message) {
-        sendEmail(message);
         LOGGER.error(message);
     }
 
     public static void error(String message, Throwable e) {
-        sendEmail(message, e);
         LOGGER.error(message, e);
     }
 
     public static void error(Throwable e) {
-        String message = Utils.stackTraceToString(e);
-        sendEmail(message, e);
         LOGGER.error(e.getMessage(), e);
-    }
-
-    public static void errorLocalOnly(final Throwable t) {
-        LOGGER.error(t.getMessage(), t);
-    }
-
-    public static boolean isDebugEnabled() {
-        return LOGGER.isDebugEnabled();
     }
 
     public static void warn(String message) {
@@ -52,40 +33,5 @@ public class Logger {
 
     public static void debug(String message) {
         LOGGER.debug(message);
-    }
-
-    private static void sendEmail(String message, Throwable e) {
-        final String sendEmailConfig;
-        final String emailPrefix;
-        if (e instanceof HibernateException) {
-            return;
-        }
-
-        // A transaction may not be started, or is already rolled back. Must wrap calls here
-        //  in a try-catch to avoid uncaught errors in error-handling code.
-        try {
-            sendEmailConfig = Utils.getConfigValue(ConfigurationKey.SEND_EMAIL_ON_ERRORS);
-            emailPrefix =  Utils.getConfigValue(ConfigurationKey.ERROR_EMAIL_EXCEPTION_PREFIX);
-        } catch (Throwable t) {
-            LOGGER.error("Cannot load email configuration to mail errors!", t);
-            return;
-        }
-
-        // Do not allow errors constructing the email to abort error handler logging
-        try {
-            if (sendEmailConfig != null && sendEmailConfig.equalsIgnoreCase("YES")) {
-                SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                String body = "System Time: " + dateFormatter.format((new Date())) + "\n\n";
-                body = body + message;
-                String subject = "Error";
-                EmailFactory.getEmail().sendError(emailPrefix + " " + subject, body);
-            }
-        } catch(Throwable t) {
-            LOGGER.error("Failed to build error mail!", t);
-        }
-    }
-
-    private static void sendEmail(String msg) {
-        sendEmail(msg, new Exception("Error"));
     }
 }
