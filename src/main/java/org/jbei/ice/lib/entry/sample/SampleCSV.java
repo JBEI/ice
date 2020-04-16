ba3ab70ac01dd5227ce786864f562716dfa0ba6a
Hector Plahar <haplahar@lbl.gov>
2020-04-16 13:57:46 -0700

    updating packages/dependencies

--- src/main/java/org/jbei/ice/lib/entry/sample/SampleCSV.java
+++ src/main/java/org/jbei/ice/lib/entry/sample/SampleCSV.java
@@ -2,6 +2,8 @@ package org.jbei.ice.lib.entry.sample;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
 import org.apache.commons.lang3.StringUtils;
 import org.jbei.ice.lib.common.logging.Logger;
 import org.jbei.ice.lib.dto.ConfigurationKey;
@@ -48,34 +50,38 @@ public class SampleCSV implements Closeable {
 
     public List<String> parse() throws IOException {
         List<String> errors = new ArrayList<>();
-        String[] header = reader.readNext();
+        try {
+            String[] header = reader.readNext();
 
-        for (String[] line : reader) {
-            String entryId = line[0];
-            if (StringUtils.isEmpty(entryId)) {
-                continue;
-            }
+            for (String[] line : reader) {
+                String entryId = line[0];
+                if (StringUtils.isEmpty(entryId)) {
+                    continue;
+                }
 
-            if (hasEntry.getEntry(entryId) == null) {
-                Logger.error("Could not retrieve entry by id " + entryId);
-                errors.add(entryId);
-                continue;
-            }
+                if (hasEntry.getEntry(entryId) == null) {
+                    Logger.error("Could not retrieve entry by id " + entryId);
+                    errors.add(entryId);
+                    continue;
+                }
 
-            String barcode = line[2];
-            String wellLocation = line[1];
-            String plate = header[2];
+                String barcode = line[2];
+                String wellLocation = line[1];
+                String plate = header[2];
 
-            if (createSample(entryId, barcode, wellLocation, plate, entryId) == null)
-                errors.add(entryId);
+                if (createSample(entryId, barcode, wellLocation, plate, entryId) == null)
+                    errors.add(entryId);
 
-            if (header.length > 3) {
-                for (int i = 3; i < header.length; i += 1) {
-                    if (createSample(entryId, line[i], wellLocation, header[i], (entryId + " backup " + (i - 2))) == null) {
-                        errors.add(entryId);
+                if (header.length > 3) {
+                    for (int i = 3; i < header.length; i += 1) {
+                        if (createSample(entryId, line[i], wellLocation, header[i], (entryId + " backup " + (i - 2))) == null) {
+                            errors.add(entryId);
+                        }
                     }
                 }
             }
+        } catch (CsvException e) {
+            throw new IOException(e);
         }
         return errors;
     }
@@ -154,46 +160,50 @@ public class SampleCSV implements Closeable {
     }
 
     public String generate() throws IOException {
-        String[] header = reader.readNext();
-        int headerLength = header.length;
-        EntryDAO dao = DAOFactory.getEntryDAO();
-        Path path = Paths.get(Utils.getConfigValue(ConfigurationKey.TEMPORARY_DIRECTORY), UUID.randomUUID().toString());
-        FileWriter fileWriter = new FileWriter(path.toFile());
-        BufferedWriter writer = new BufferedWriter(fileWriter);
-        CSVWriter csvWriter = new CSVWriter(writer);
+        try {
+            String[] header = reader.readNext();
+            int headerLength = header.length;
+            EntryDAO dao = DAOFactory.getEntryDAO();
+            Path path = Paths.get(Utils.getConfigValue(ConfigurationKey.TEMPORARY_DIRECTORY), UUID.randomUUID().toString());
+            FileWriter fileWriter = new FileWriter(path.toFile());
+            BufferedWriter writer = new BufferedWriter(fileWriter);
+            CSVWriter csvWriter = new CSVWriter(writer);
 
-        csvWriter.writeNext(new String[]{"Well", "PartID (Strain)"});
+            csvWriter.writeNext(new String[]{"Well", "PartID (Strain)"});
 
-        for (String[] line : reader) {
-            if (line.length != headerLength) {
-                Logger.info("Skipping line");
-                continue;
+            for (String[] line : reader) {
+                if (line.length != headerLength) {
+                    Logger.info("Skipping line");
+                    continue;
+                }
+
+                String alias = line[0];
+                String name = line[1];
+                List<Entry> entries = dao.getMatching(name, alias, EntryType.PLASMID);
+                if (entries.size() != 1) {
+                    Logger.error(entries.size() + " found for " + name + " " + alias);
+                    continue;
+                }
+
+                Entry plasmid = entries.get(0);
+                List<Entry> parents = dao.getParents(plasmid.getId());
+                if (parents.size() != 1) {
+                    Logger.error("Found " + parents.size() + " for " + name + " " + alias);
+                    continue;
+                }
+
+                Entry strain = parents.get(0);
+                String locationLine = line[headerLength - 1];
+                locationLine = locationLine.substring(locationLine.lastIndexOf('-') + 1);
+                if (locationLine.length() == 2)
+                    locationLine = locationLine.charAt(0) + "0" + locationLine.charAt(1);
+                csvWriter.writeNext(new String[]{locationLine, strain.getPartNumber()});
             }
-
-            String alias = line[0];
-            String name = line[1];
-            List<Entry> entries = dao.getMatching(name, alias, EntryType.PLASMID);
-            if (entries.size() != 1) {
-                Logger.error(entries.size() + " found for " + name + " " + alias);
-                continue;
-            }
-
-            Entry plasmid = entries.get(0);
-            List<Entry> parents = dao.getParents(plasmid.getId());
-            if (parents.size() != 1) {
-                Logger.error("Found " + parents.size() + " for " + name + " " + alias);
-                continue;
-            }
-
-            Entry strain = parents.get(0);
-            String locationLine = line[headerLength - 1];
-            locationLine = locationLine.substring(locationLine.lastIndexOf('-') + 1);
-            if (locationLine.length() == 2)
-                locationLine = locationLine.charAt(0) + "0" + locationLine.charAt(1);
-            csvWriter.writeNext(new String[]{locationLine, strain.getPartNumber()});
+            csvWriter.close();
+            return path.getFileName().toString();
+        } catch (CsvValidationException e) {
+            throw new IOException(e);
         }
-        csvWriter.close();
-        return path.getFileName().toString();
     }
 
     protected PartSample createSample(String entryId, String barcode, String wellLocation, String plateName,
