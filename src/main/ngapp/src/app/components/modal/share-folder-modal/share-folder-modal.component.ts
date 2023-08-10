4ab181492d4ed5558ab412e70d62a4570170dab4
Hector Plahar <hector.plahar@gmail.com>
2023-08-10 12:39:22 -0700

    start of share folder modal; currently retrieves any associated permissions

--- src/main/ngapp/src/app/components/modal/share-folder-modal/share-folder-modal.component.ts
+++ src/main/ngapp/src/app/components/modal/share-folder-modal/share-folder-modal.component.ts
@@ -2,6 +2,7 @@ import {Component} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {HttpService} from "../../../services/http.service";
 import {Folder} from "../../../models/folder";
+import {Permission} from "../../../models/permission";
 
 @Component({
     selector: 'app-share-folder-modal',
@@ -12,10 +13,20 @@ export class ShareFolderModalComponent {
 
     folder: Folder;
     folderPermissionsOnly: boolean;
+    permissions: Permission[];
 
     constructor(public activeModal: NgbActiveModal, private http: HttpService) {
     }
 
     ngOnInit(): void {
+        this.http.get('folders/' + this.folder.id + '/permissions').subscribe({
+            next: (result: Permission[]) => {
+                console.log(result);
+                this.permissions = result;
+
+            }, error: (error: any) => {
+
+            }
+        })
     }
 }
